8e0f73ae8441e0582d2ff77c2ef6d35977354b92
Bozhik Nazar <60932339+bozhik45@users.noreply.github.com>
2020-02-16 15:50:47 +0200

    Update and rename HW1_NazarBozhik to HW1_NazarBozhik.cpp

--- HomeWork1/HW1_NazarBozhik.cpp
+++ HomeWork1/HW1_NazarBozhik.cpp
@@ -3,50 +3,55 @@
 
 int main() {
   std::cout << "Please enter three numbers in range [2..255]\n";
-  const int cF = 4;
-  const int cT = 2;
+
   float a, b, c;
   std::cout << "Enter a =";
   std::cin >> a;
-  while (a <= 2 || a >= 255) {
-    std::cout << "Enter a valid numder[2..255]\n";
+  const int min = 2;
+  const int max = 255;
+  while (a <= min || a >= max) {
+    std::cout << "Enter c valid numder[2..255]\nEnter a =";
+    std::cin >> a;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Enter a =";
-    std::cin >> a;
   }
   std::cout << "Enter b =";
   std::cin >> b;
-  while (b <= 2 || b >= 255) {
-    std::cout << "Enter a valid numder[2..255]\n";
+  while (b <= min || b >= max) {
+    std::cout << "Enter c valid numder[2..255]\nEnter b =";
+    std::cin >> b;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Enter b =";
-    std::cin >> b;
   }
   std::cout << "Enter c =";
   std::cin >> c;
-  while (c <= 2 || c >= 255) {
-    std::cout << "Enter c valid numder[2..255]\n";
+  while (c <= min || c >= max) {
+    std::cout << "Enter c valid numder[2..255]\nEnter c =";
+    std::cin >> c;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Enter c =";
-    std::cin >> c;
   }
-  auto D = pow(b, 2) - cF * a * c;
+
+  const int cF = 4;
+  const int cT = 2;
+
+  const auto D = pow(b, 2) - cF * a * c;
 
   // std::cout << sqrt(D); //проверял роботу корня
+
   if (D > 0) {
     std::cout << "We will have x1 and x2, because our D > 0\n";
-    const auto xf = -b - sqrt(D) / cT * a;
+    const auto xf = (-b - sqrt(D)) / (cT * a);
     std::cout << "x1 =" << xf << "\n";
-    const auto xd = -b + sqrt(D) / cT * a;
+    const auto xd = (-b + sqrt(D)) / (cT * a);
     std::cout << "x2 =" << xd << "\n";
-  } else if (D == 0) {
+  }
+  if (D == 0) {
     std::cout << "We will have just one x, because our D = 0\n";
-    const auto xs = -b / cT * a;
+    const auto xs = -b / (cT * a);
     std::cout << "x =" << xs;
-  } else {
+  }
+  if (D < 0) {
     std::cout << "D < 0, so we don't have variants";
   }
 
