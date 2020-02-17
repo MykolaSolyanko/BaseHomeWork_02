6c27ae30db7b0a3de05826e2de91223c3d66e786
Bozhik Nazar <60932339+bozhik45@users.noreply.github.com>
2020-02-17 17:24:33 +0100

    Update HW1_NazarBozhik.cpp

--- HomeWork1/HW1_NazarBozhik.cpp
+++ HomeWork1/HW1_NazarBozhik.cpp
@@ -2,31 +2,34 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Please enter three numbers in range [2..255]\n";
-
-  float a, b, c;
-  std::cout << "Enter a =";
-  std::cin >> a;
   const int min = 2;
   const int max = 255;
+  std::cout << "Please enter three numbers in range [" << min << ".. " << max
+            << "]\n";
+
+  float a;
+  std::cout << "Enter a =";
+  std::cin >> a;
   while (a <= min || a >= max) {
-    std::cout << "Enter c valid numder[2..255]\nEnter a =";
+    std::cout << "Enter valid numder[" << min << ".. " << max << "]\nEnter a =";
     std::cin >> a;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   }
+  float b;
   std::cout << "Enter b =";
   std::cin >> b;
   while (b <= min || b >= max) {
-    std::cout << "Enter c valid numder[2..255]\nEnter b =";
+    std::cout << "Enter valid numder[" << min << ".. " << max << "]\nEnter b =";
     std::cin >> b;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   }
+  float c;
   std::cout << "Enter c =";
   std::cin >> c;
   while (c <= min || c >= max) {
-    std::cout << "Enter c valid numder[2..255]\nEnter c =";
+    std::cout << "Enter valid numder[" << min << ".. " << max << "]\nEnter c =";
     std::cin >> c;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -45,14 +48,17 @@ int main() {
     std::cout << "x1 =" << xf << "\n";
     const auto xd = (-b + sqrt(D)) / (cT * a);
     std::cout << "x2 =" << xd << "\n";
+    return 0;
   }
   if (D == 0) {
     std::cout << "We will have just one x, because our D = 0\n";
     const auto xs = -b / (cT * a);
     std::cout << "x =" << xs;
+    return 0;
   }
   if (D < 0) {
     std::cout << "D < 0, so we don't have variants";
+    return 0;
   }
 
   return 0;
