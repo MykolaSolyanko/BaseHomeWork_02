20426c84cb83b27e4515d69e58d7998cac064b8c
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-02-19 23:44:56 +0200

    Update Nataliia_Kuchynska_HW1.cpp

--- HomeWork1/Nataliia_Kuchynska_HW1.cpp
+++ HomeWork1/Nataliia_Kuchynska_HW1.cpp
@@ -5,8 +5,8 @@ int main() {
 
   long int a{}, b{}, c{};
 
-  long min_val = std::numeric_limits<int>::min();
-  long max_val = std::numeric_limits<int>::max();
+  long const min_val = std::numeric_limits<int>::min();
+  long const max_val = std::numeric_limits<int>::max();
 
   std::cout << "To solve a quadratic equation \n\tA*X^2+B*X+C=0" << std::endl;
   std::cout << "Please, enter integers A, B and C (in the range from "
@@ -14,34 +14,35 @@ int main() {
 
   std::cout << "A = ";
   std::cin >> a;
-
-  std::cout << "B = ";
-  std::cin >> b;
-
-  std::cout << "C = ";
-  std::cin >> c;
-
-  if (a == 0 || a < min_val || a > max_val || b < min_val || b > max_val ||
-      c < min_val || c > max_val) {
+  if (a == 0 || a < min_val || a > max_val) {
     std::cout << "Wrong initial data" << std::endl;
     return 1;
   }
-  long double d{};
-  d = b * b - 4 * a * c;
-  long double x1{}, x2{};
-  if (d >= 0) {
-    x1 = (-b + sqrt(d)) / (2 * a);
-    x2 = (-b - sqrt(d)) / (2 * a);
+  std::cout << "B = ";
+  std::cin >> b;
+  if (b < min_val || b > max_val) {
+    std::cout << "Wrong initial data" << std::endl;
+    return 1;
+  }
+  std::cout << "C = ";
+  std::cin >> c;
+
+  if (c < min_val || c > max_val) {
+    std::cout << "Wrong initial data" << std::endl;
+    return 1;
+  }
+  long double d = b * b - 4 * a * c;
+  long double x1 = (-b + sqrt(d)) / (2 * a);
+  long double x2 = (-b - sqrt(d)) / (2 * a);
+  if (d == 0) {
     std::cout << "The equation has one solution X = " << x1
               << " of multiplicity 2 " << std::endl;
-  } else {
+  } else if (d > 0) {
     std::cout << "The equation has two solutions: \nX1 = " << x1
               << "\nX2 =" << x2 << std::endl;
+  } else {
+    std::cout << "The equation has no solutions" << std::endl;
   }
-}
-else {
-  std::cout << "The equation has no solutions" << std::endl;
-}
 
-return 0;
+  return 0;
 }
