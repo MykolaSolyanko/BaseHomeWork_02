fddd249a1c463d1ad35b20c67835bce18cfa01cc
KravchenkoBogdan <60968557+KravchenkoBogdan@users.noreply.github.com>
2020-02-19 18:44:50 +0200

    Create Bogdan_Kravchenko_HW2_ex1.cpp

---
+++ Bogdan_Kravchenko_HW2_ex1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+  int number;
+
+  std::cout << "Please, enter any number\n" << std::endl;
+  std::cout << "This is arithmetic mean of your digits number" << std::endl;
+  std::cin >> number;
+
+  int sum = 0;
+  int count = 0;
+
+  if (number != 0) {
+
+    while (number != 0) {
+      sum += number % 10;
+      number /= 10;
+      count++;
+    }
+
+    auto arithmetic_mean = static_cast<double>(sum) / count;
+    std::cout << "The sum of digits your number is " << sum << std::endl;
+    std::cout << "The arithmetic mean of digits your number is "
+              << arithmetic_mean << std::endl;
+
+  } else
+    std::cout << "Error! Enter number more then zero!" << std::endl;
+
+  return 0;
+}
