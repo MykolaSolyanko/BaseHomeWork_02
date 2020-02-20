944075f6b7ba247cefabccd89f582e4f09c3ee35
Sam4uk <sto.ukr.net@gmail.com>
2020-02-20 22:18:55 +0200

    Make menu for one sub programm

--- HomeWork2/Taras_Samchuk_HW2.cpp
+++ HomeWork2/Taras_Samchuk_HW2.cpp
@@ -4,7 +4,50 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
-int main(/*int argc, char const *argv[]*/)
+//=================================================================================
+//    INTRO
+//=================================================================================
+int Intro(void)
+{
+  std::cout << R"(
+  +------------------+
+)";
+  return 0;
+}
+//=================================================================================
+//    CONSTANTS
+//=================================================================================
+const int Names_langth=12;
+const char Items[][Names_langth]{
+    "Digits summ"
+};
+enum{LABEL_EXIT,
+    LABEL_DIGITS_SUMM,
+}
+const int Items_Count = sizeof(Items) / sizeof(Items[0]);
+//=================================================================================
+//    MENU TO SCREEN
+//=================================================================================
+int PrintMenu()
+{
+  bool TryAgain {true};
+  size_t MyChoice;
+  do{
+    std::cout << "\tSelect a task"<<std::endl;
+    for (size_t i = 0; i < Items_Count; i++)
+      std::cout <<"\t("<<i+1<<")  "<< Items[i]<<std::endl;
+    std::cout<<"\tEnter the number from 0 to "<<Items_Count<<" and press [ENTER]\n\tType 0 for exit"<<std::endl;
+    std::cin>>MyChoice;
+    TryAgain=0 > MyChoice || MyChoice > Items_Count;
+    if (!TryAgain)break;
+  }while (TryAgain);
+  return MyChoice;
+}
+//=================================================================================
+//    SUB PROGRAMMs
+//=================================================================================
+
+int DigitsSumm()
 {
   std::cout << R"(
       +--------------------------------+
@@ -18,9 +61,9 @@ int main(/*int argc, char const *argv[]*/)
   // 10 is the minimum two-digit number
   const size_t k_min_limit = 10;
   // "almost max" for this type
-  const size_t k_max_limit = std::numeric_limits<size_t>::max()>>1;
+  const size_t k_max_limit = std::numeric_limits<size_t>::max() >> 1;
   size_t Number;
-  bool Incorrect;//true=incorrect; false=correct;
+  bool Incorrect; //true=incorrect; false=correct;
   //We get the number
   do
   {
@@ -53,7 +96,31 @@ int main(/*int argc, char const *argv[]*/)
     Digits_Count++;
   } while (Number > pow(k_digit_10, Digits_Count));
   const double arithmetic_mean = (Digits_Summ + 0.f) / Digits_Count;
-  std::cout << ") have the sum equal " << Digits_Summ << 
-               " and arithmetic mean " <<arithmetic_mean<< std::endl;
+  std::cout << ") have the sum equal " << Digits_Summ << " and arithmetic mean " << arithmetic_mean << std::endl;
+  return 0;
+}
+
+//=================================================================================
+//    MAIN PROGRAMM
+//=================================================================================
+int main()
+{
+  do
+  {
+    std::cout<<std::endl;
+    switch (PrintMenu())
+    {
+    case 0:
+      return 0;
+      break;
+    case 1:
+        DigitsSumm();
+      break;
+    
+    default:
+      return 0;
+      break;
+    }
+  } while (true);
   return 0;
 }
