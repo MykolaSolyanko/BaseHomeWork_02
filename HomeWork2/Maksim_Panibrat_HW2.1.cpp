7405373373080fd61996f5f5e60685f6dcbb3a03
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-02-23 19:15:41 +0200

    Update Maksim_Panibrat_HW2.1.cpp

--- HomeWork2/Maksim_Panibrat_HW2.1.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.1.cpp
@@ -1,62 +1,23 @@
 #include <iostream>
 
 int main() {
-  std::cout << "x1= a1 + a2 + ... + an" << std::endl;
-  std::cout << "x2=x1/n\n";
-  std::cout << "Enter 'n' value, max=5 " << std::endl;
-  int n;
-  std::cin >> n;
-  while (n > 5) {
+  std::cout << "This program breaks a number into digits and displays a sum of "
+               "those digits with their arithmetic mean.\n";
+  std::cout << "Enter number\n";
+  int num;
+  std::cin >> num;
+  while (num == 0) {
     std::cout << "Out of range. Try again\n";
-    std::cin >> n;
+    std::cin >> num;
   }
-  int a1, a2, a3, a4, a5;
-  if (n == 1) {
-    std::cout << "Enter 'a1' value\n";
-    std::cin >> a1;
-    float x1 = a1;
+  float sum = 0, digits = 0;
+  while (num) {
+    sum += (num % 10);
+    num /= 10;
+    digits++;
   }
-  if (n == 2) {
-    std::cout << "Enter 'a1' value\n";
-    std::cin >> a1;
-    std::cout << "Enter 'a2' value\n";
-    std::cin >> a2;
-    float x1 = a1 + a2;
-  }
-  if (n == 3) {
-    std::cout << "Enter 'a1' value\n";
-    std::cin >> a1;
-    std::cout << "Enter 'a2' value\n";
-    std::cin >> a2;
-    std::cout << "Enter 'a3' value\n";
-    std::cin >> a3;
-    float x1 = a1 + a2 + a3;
-  }
-  if (n == 4) {
-    std::cout << "Enter 'a1' value\n";
-    std::cin >> a1;
-    std::cout << "Enter 'a2' value\n";
-    std::cin >> a2;
-    std::cout << "Enter 'a3' value\n";
-    std::cin >> a3;
-    std::cout << "Enter 'a4' value\n";
-    std::cin >> a4;
-    float x1 = a1 + a2 + a3 + a4;
-  }
-  if (n == 5) {
-    std::cout << "Enter 'a1' value\n";
-    std::cin >> a1;
-    std::cout << "Enter 'a2' value\n";
-    std::cin >> a2;
-    std::cout << "Enter 'a3' value\n";
-    std::cin >> a3;
-    std::cout << "Enter 'a4' value\n";
-    std::cin >> a4;
-    std::cout << "Enter 'a5' value\n";
-    std::cin >> a5;
-    float x1 = a1 + a2 + a3 + a4 + a5;
-  }
-  std::cout << "x1=" << x1 << std::endl;
-  double x2 = x1 / n;
-  std::cout << "x2=" << x2 << std::endl;
+  std::cout << "Sum of digits = " << sum << std::endl;
+  float ar_m = sum / digits;
+  std::cout << "Arithmetic mean = " << ar_m << std::endl;
   return 0;
+}
