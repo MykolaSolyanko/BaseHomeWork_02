ae2931471fb3bcec562ce184b937c6a9ac65ef5e
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-02-24 11:34:17 +0200

    Update Maksim_Panibrat_HW2.3.cpp

--- HomeWork2/Maksim_Panibrat_HW2.3.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.3.cpp
@@ -10,14 +10,15 @@ int main() {
   int32_t num;
   std::cin >> num;
   int64_t rev_num{0};
-  if ((num / 10) != 0) {
-    do {
-      rev_num *= 10;
-      rev_num += num % 10;
-      num /= 10;
-    } while (num != 0);
-  } else
-    rev_num = num;
+  if (num < 10) {
+    std::cout << "reverse number is the same as you are entered\n";
+  }
+  do {
+    rev_num *= 10;
+    rev_num += num % 10;
+    num /= 10;
+  } while (num != 0);
+
   std::cout << rev_num << "\n";
   return 0;
 }
