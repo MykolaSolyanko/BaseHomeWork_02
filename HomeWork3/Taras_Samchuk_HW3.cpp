1c1491578b13e36dbef0f7586d8b8a0177b5de17
Sam4uk <sto.ukr.net@gmail.com>
2020-02-26 13:25:44 +0200

    save 13:25

--- HomeWork3/Taras_Samchuk_HW3.cpp
+++ HomeWork3/Taras_Samchuk_HW3.cpp
@@ -9,29 +9,18 @@ void SwapChar(char *first_ptr, char *second_ptr)
     *second_ptr=*tmp_ptr;
 
 }
-/* int Revers()
-{
-    char InputStr[25]{};
-    std::cout<<"input=";
-    std::cin>>InputStr;
-    size_t len{0};
-    while(InputStr[len])len++;
-    std::cout<<len;
-    return 0;
-} */
-int Input(auto *input)
-{
-    return 0;
-}
 int main()
 {
+    char Test[]{"123456789"};
+    size_t TestLength = sizeof(Test)-1;
+    for (size_t i = 0; i < TestLength/2; i++)
+    {
+        SwapChar(&Test[i],&Test[TestLength-1-i]);
+        std::cout<<Test<<std::endl;
+    }
 
-/*     char a='a';
-    char b='b';
-    std::cout<<a<<b<<std::endl;
-    Swap(&a,&b);
+    
 
-    std::cout<<a<<b<<std::endl; */
 
     return 0;
 }
