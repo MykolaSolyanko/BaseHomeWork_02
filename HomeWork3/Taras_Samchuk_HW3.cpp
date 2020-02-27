aa542a8ccdab65724e0090022962e31bae80f00d
Sam4uk <sto.ukr.net@gmail.com>
2020-02-27 15:54:20 +0200

    save 15:54

--- HomeWork3/Taras_Samchuk_HW3.cpp
+++ HomeWork3/Taras_Samchuk_HW3.cpp
@@ -1,26 +1,37 @@
-//Working with pointers
+// Working with pointers
 #include <iostream>
-void SwapChar(char *first_ptr, char *second_ptr)
-{
-    char tmp='\0';
-    char *tmp_ptr=&tmp;
-    *tmp_ptr=*first_ptr;
-    *first_ptr=*second_ptr;
-    *second_ptr=*tmp_ptr;
-
+int SwapChar(char *const first_ptr, char *const second_ptr) {
+  if (first_ptr == nullptr || second_ptr == nullptr ||
+      first_ptr == second_ptr) {
+    return 1; // ERROR
+  }
+  // char tmp = '\0';
+  // char *tmp_ptr = &tmp;
+  char tmp_ptr = *first_ptr;
+  *first_ptr = *second_ptr;
+  *second_ptr = tmp_ptr;
+  return 0; // OK
 }
-int main()
-{
-    char Test[]{"123456789"};
-    size_t TestLength = sizeof(Test)-1;
-    for (size_t i = 0; i < TestLength/2; i++)
-    {
-        SwapChar(&Test[i],&Test[TestLength-1-i]);
-        std::cout<<Test<<std::endl;
-    }
-
-    
-
-
-    return 0;
+int Reverse(char *text) {
+  if (text == nullptr) {
+    return 1; // Error
+  }
+  size_t len{0};
+  while (text[len] != '\0')
+    len++;
+  for (size_t i = 0; i < len / 2; i++) {
+    if (!SwapChar(&text[i], &text[len - 1 - i]))
+      return 0;
+  }
+  return 0;
+}
+int main() {
+  char test[]{"test string-"};
+  std::cout << "";
+  std::cin >> test;
+  char *test_ptr = test;
+  std::cout << test << std::endl;
+  Reverse(test_ptr);
+  std::cout << test << std::endl;
+  return 0;
 }
