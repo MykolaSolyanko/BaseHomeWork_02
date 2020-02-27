c85e45b8a5bbddbda56a50003915c532717f37bd
SerghiiKhirniy <khirniy.s@gmail.com>
2020-02-27 13:00:33 +0200

    Create S.KhirniyHw2_Christmas Three.cpp

---
+++ HomeWork2/S.KhirniyHw2_Christmas Three.cpp
@@ -0,0 +1,36 @@
+// Christmas Three.cpp
+
+#include <iostream>
+
+int main() {
+  char z = '*', space = ' ';
+  int n;
+  std::cin >> n;
+
+  n--;
+  bool f = false;
+  for (int i = 1; i <= n; i++) {
+    if (i >= 2 && !f)
+      f = true;
+
+    for (int j = 1; j <= n - i; j++)
+      std::cout << space;
+
+    for (int j = n - i; j < n; j++)
+      std::cout << z;
+
+    if (f)
+      for (int j = 1; j < i; j++)
+        std::cout << z;
+
+    std::cout << std::endl;
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if (i == n) {
+      std::cout << z;
+      break;
+    }
+    std::cout << space;
+  }
+}
