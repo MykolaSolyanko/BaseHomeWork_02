253ec7308c0cc3ce5767db84d8c0354e3af87c7f
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-02-28 23:20:23 +0200

    Update Maksim_Panibrat_HW2.3.cpp

--- HomeWork2/Maksim_Panibrat_HW2.3.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.3.cpp
@@ -4,9 +4,9 @@
 int main() {
   std::cout << "This program turns number in reverse.\n";
   std::cout << "Enter number to reverse it\n";
-  int32_t num;
+  int num;
   std::cin >> num;
-  int64_t rev_num{0};
+  int rev_num{0};
   if (num < 10) {
     std::cout << "reverse number is the same as you are entered\n";
   }
