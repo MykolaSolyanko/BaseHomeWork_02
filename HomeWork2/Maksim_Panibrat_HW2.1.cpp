8cf69e8fd214a569cb8e54057fd2380030b6912c
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-02-28 23:23:26 +0200

    Update Maksim_Panibrat_HW2.1.cpp

--- HomeWork2/Maksim_Panibrat_HW2.1.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.1.cpp
@@ -4,21 +4,21 @@ int main() {
   std::cout << "This program breaks a number into digits and displays a sum of "
                "those digits with their arithmetic mean.\n";
   std::cout << "Enter number\n";
-  int num;
-  std::cin >> num;
-  while (num == 0) {
+  int number;
+  std::cin >> number;
+  while (number == 0) {
     std::cout << "Out of range. Try again\n";
-    std::cin >> num;
+    std::cin >> number;
   }
   float sum = 0;
   int digits {0};
-  while (num) {
-    sum += (num % 10);
-    num /= 10;
+  while (number) {
+    sum += (number % 10);
+    number /= 10;
     digits++;
   }
   std::cout << "Sum of digits = " << sum << std::endl;
-  float ar_m = sum / digits;
-  std::cout << "Arithmetic mean = " << ar_m << std::endl;
+  float arithmetic_mean = sum / digits;
+  std::cout << "Arithmetic mean = " << arithmetic_mean << std::endl;
   return 0;
 }
