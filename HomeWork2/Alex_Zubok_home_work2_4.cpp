5b9f7d1d286ec09fa75ee2405aa52919905b6823
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-03 15:08:47 +0200

    ver 1.1

--- HomeWork2/Alex_Zubok_home_work2_4.cpp
+++ HomeWork2/Alex_Zubok_home_work2_4.cpp
@@ -5,14 +5,14 @@ bool range_check( long long x, int a, int b ){
 }
 
 bool modul_remainder( int value, unsigned int modul_value ){
-    return ( value % modul_value ) == 0;
+    return ( value % modul_value ) > 0;
 }
 
 void check_valid_input( int &value, int a, int b ){
     while ( !( std::cin >> value ) || ! ( range_check( value, a, b ) ) ) {
         std::cin.clear();
         std::cin.ignore( std::numeric_limits<int>::max(), '\n' );
-        std::cout << "Sorry, you enter invalid number of quantity inputs, please enter again[" << a << "-" << b << "]: ";
+        std::cout << "Sorry, you enter invalid number of quantity inputs, please enter again[" << a << " - " << b << "]: ";
     }
 }
 
@@ -22,7 +22,7 @@ int main(){
     const int kMax_inputs = 90, kMax_range = 50;
     std::cout << "Please enter number of quantity inputs number from 1 till 50: ";
     check_valid_input( iter, kMin_range, kMax_range );
-    std::cout << "Please enter " << iter << " inputs." << std::endl;
+    std::cout << "Please enter " << iter << " inputs[-60 - 90]." << std::endl;
     long long summ{};
     const int kTwo = 2;
     for ( size_t index = 0; index < iter; index++){
