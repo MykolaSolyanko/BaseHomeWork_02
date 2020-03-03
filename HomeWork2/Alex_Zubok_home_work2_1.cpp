c06e9abf60c88086757d2c277e5538060c11f34f
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-03 11:30:50 +0200

    up ver 1.1

--- HomeWork2/Alex_Zubok_home_work2_1.cpp
+++ HomeWork2/Alex_Zubok_home_work2_1.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 int main() 
 {
-    long long var {}, summ_var {};
+    unsigned long long var {}, summ_var {};
     std::cout << "Please enter integer value:";
     std::cin >> var;
     size_t index{};
-    for ( size_t tmp = var, summ {}, iter {}; tmp > 0; tmp/=10) {
+    for ( size_t tmp = var, summ {}, iter {}; tmp > 0; tmp /= 10 ) {
         summ += tmp % 10;
         ++iter;
         index = iter;
         summ_var = summ;
     }
-    std::cout << "Summ of " << var << " value = " << summ_var << std::endl;
-    std::cout << "Arithmetic means of " << var << " value = " << ( summ_var / index ) << std::endl;
+    std::cout << "Summ of ";
+    for ( size_t tmp = var, i = index; i > 0; i-- ){
+        int tmp_num = tmp / pow( 10, i - 1 );
+        std::cout << tmp_num;
+        tmp -= tmp_num * pow( 10, i - 1 );
+        if ( i < 2 ) {
+            std::cout << " = ";
+        }
+        else {
+            std::cout << " + ";
+        }
+    }
+    std::cout << summ_var << std::endl;
+    std::cout << "Arithmetic means of " << summ_var << " / " << index << " = " << ( summ_var / index ) << std::endl;
     return 0;
 }
