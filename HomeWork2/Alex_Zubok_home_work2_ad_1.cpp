b629fc751cd52439ef5ac91da92fe151ae9b2d69
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-04 15:33:12 +0200

    Alex_Zubok_home_work2_ad_1 ver 1

---
+++ HomeWork2/Alex_Zubok_home_work2_ad_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	long long value {};
+	const int kZero = 0;
+	const int kOne = 1;
+	const int kTwo = 2;
+	std::cin >> value;
+	if ( ( value % kTwo ) == kZero )
+		++value;
+	size_t index {};
+	while( value > kZero ) {
+		value -= kTwo;
+		++index;
+	}
+	for ( size_t i_ind = index, t_ind = kOne; i_ind > kZero; i_ind--, t_ind+=kTwo ) {
+		if ( i_ind - kOne > kZero ) {
+			for ( size_t j_ind = i_ind - kOne; j_ind > kZero; j_ind-- ) {
+				std::cout << " ";
+			}
+		}
+		for ( size_t k_ind = t_ind; k_ind > kZero; k_ind-- ) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
