33684b46e70846e886124a657087b931befcd7f4
SerghiiKhirniy <khirniy.s@gmail.com>
2020-03-05 19:01:23 +0200

    Create Khirniy_S_Sorting_array_hw3_ptr.cpp

---
+++ HomeWork3/Khirniy_S_Sorting_array_hw3_ptr.cpp
@@ -0,0 +1,59 @@
+// Sorting array
+
+#include <iostream>
+
+// Function for add numbers to array
+void addToArray(int *begin, int *end) {
+  for (size_t i{0}; i < *end; i++) {
+    std::cin >> *begin;
+    begin++;
+  }
+}
+// Function for print to screen
+void printToScreen(int *begin, int *end) {
+  for (size_t i{0}; i < *end; i++) {
+    std::cout << *begin << " ";
+    begin++;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  const size_t arrSize{6};
+  int a[arrSize];
+
+  int *begin = a;
+  int *end = a + arrSize;
+
+  // Input array from keyboard
+  std::cout << "Input " << arrSize << " elements in array: " << std::endl;
+  addToArray(begin, end);
+  std::cout << std::endl;
+
+  // Output array before sorting
+  std::cout << "Array has " << arrSize << " elements: " << std::endl;
+  printToScreen(begin, end);
+  std::cout << std::endl;
+
+  int temp; // Temporary variable for swapping elements
+
+  // Sorting array
+  for (size_t i{0}; i < arrSize - 1; i++) {
+    for (size_t j{0}; j < arrSize - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        // Moove elements
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
+    }
+  }
+
+  // Output array after sorting
+  std::cout << std::endl;
+  std::cout << "Sorting array of " << arrSize << " elements: " << std::endl;
+
+  printToScreen(begin, end);
+  std::cout << std::endl;
+  std::cout << std::endl;
+  return 0;
+}
