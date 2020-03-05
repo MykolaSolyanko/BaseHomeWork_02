e1bb295e2f1db89f92bfe6afbb9bba684e5652b4
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-05 21:56:46 +0200

    Alex_Zubok_home_work1 ver 2.3

--- HomeWork1/Alex_Zubok_home_work1.cpp
+++ HomeWork1/Alex_Zubok_home_work1.cpp
@@ -6,31 +6,31 @@ int main()
 {
     long long var_a {};
     size_t kMin = 1, kMax = 500;
-    std::cout << "Please enter value for variable 'a' from 1 till 500, and press 'Enter':";
+    std::cout << "Please enter value for variable 'a' from " << kMin << " till " << kMax << ", and press 'Enter':";
     while ( !( std::cin >> var_a ) || std::cin.fail() || var_a < kMin || var_a > kMax ) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<long long>::max(), '\n');
-        std::cout << "You set incorrect value for variable 'a', please enter again variable 'a' from 1 till 500, and press 'Enter':";
+        std::cout << "You set incorrect value for variable 'a', please enter again variable 'a' from " << kMin << " till " << kMax << ", and press 'Enter':";
     }
     std::cout << "You are successfully set variable 'a' = " << var_a << std::endl;
     
     
     long long var_b {};
-    std::cout << "Please enter value for variable 'b' from 1 till 500, and press 'Enter':";
+    std::cout << "Please enter value for variable 'b' from " << kMin << " till " << kMax << ", and press 'Enter':";
     while ( ! ( std::cin >> var_b ) || std::cin.fail() || var_b < kMin || var_b > kMax ) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<long long>::max(), '\n');
-        std::cout << "You set incorrect value for variable 'b', please enter again variable 'b' from 1 till 500, and press 'Enter':";
+        std::cout << "You set incorrect value for variable 'b', please enter again variable 'b' from " << kMin << " till " << kMax << ", and press 'Enter':";
         
     }
     std::cout << "You are successfully set variable 'b' = " << var_b << std::endl;
 
     long long var_c {};
-    std::cout << "Please enter value for variable 'c' from 1 till 500, and press 'Enter':";
+    std::cout << "Please enter value for variable 'c' from " << kMin << " till " << kMax << ", and press 'Enter':";
     while ( ! ( std::cin >> var_c ) || std::cin.fail() || var_c < kMin || var_c > kMax ) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<long long>::max(), '\n');
-        std::cout << "You set incorrect value for variable 'c', please enter again variable 'c' from 1 till 500, and press 'Enter':";
+        std::cout << "You set incorrect value for variable 'c', please enter again variable 'c' from " << kMin << " till " << kMax << ", and press 'Enter':";
         
     }
     std::cout << "You are successfully set variable 'c' = " << var_c << std::endl;
