f2f003c912913dec9e342fff54759af15639f916
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-11 16:48:57 +0200

    Update Maksim_Panibrat_HW1.cpp
    Переписал код

--- HomeWork1/Maksim_Panibrat_HW1.cpp
+++ HomeWork1/Maksim_Panibrat_HW1.cpp
@@ -2,52 +2,48 @@
 #include <iostream>
 #include <limits>
 
-int main(int argc, char const *argv[]) {
-  std::cout << "x= ((-b) +- sqrt(b^2-4ac))/2a\n";
-
-  const short kMinA = 1, kMaxA = 200;
-  std::cout << "Enter 'a' number in range [" << kMinA << "..." << kMaxA << "]"
-            << "\n";
-  short a;
-  while (!(std::cin >> a), a < kMinA || a > kMaxA) {
-    std::cout << "Error, out of range or not number\n";
+int input_check(int value, const int kMinNum, const int kMaxNum,
+                const char check) {
+  std::cout << "Enter ' " << check << " ' number in range [ " << kMinNum
+            << " ... " << kMaxNum << " ]\n";
+  while (!(std::cin >> value) || value < kMinNum || value > kMaxNum) {
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Error not number or  "
+              << (value < kMinNum ? "less than minimum" : "bigger than maximum")
+              << std::endl;
   }
 
-  const short kMinB = 1, kMaxB = 340;
-  std::cout << "Enter 'b' number in range [" << kMinB << "..." << kMaxB << "]"
-            << "\n";
-  short b;
-  while (!(std::cin >> b), b < kMinB || b > kMaxB) {
-    std::cout << "Error, out of range or not number\n";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  }
+  return value;
+}
 
-  const short kMinC = 1, kMaxC = 400;
-  std::cout << "Enter 'c' number in range [" << kMinC << "..." << kMaxC << "]"
-            << "\n";
-  short c;
-  while (!(std::cin >> c), c < kMinC || c > kMaxC) {
-    std::cout << "Error, out of range or not number\n";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  }
-  double D = pow(b, 2) - 4 * a * c;
-  if (D < 0) {
-    std::cout << "No real roots\n";
+double Dis_check(double D, int a, int b, int c, double x1, double x2) {
+  const double cons4 = 4, cons2 = 2;
+  D = pow(b, 2) - (cons4 * a * c);
+  if (D <= 0) {
+    std::cout << (D < 0 ? "D<0, no real roots" : "D=0, not quadratic")
+              << std::endl;
     return 0;
   }
-  if (D == 0) {
-    std::cout << "Not quadratic\n";
-    return 0;
-  }
+  std::cout << "D>0\t Initilizating...\n";
+  x1 = ((-b) + sqrt(pow(b, 2) - (cons4 * a * c))) / (cons2 * a);
+  x2 = ((-b) - sqrt(pow(b, 2) - (cons4 * a * c))) / (cons2 * a);
+  std::cout << "x1 = " << x1 << "\n x2 = " << x2 << std::endl;
+  return x1, x2;
+}
 
-  std::cout << "There 2 roots\n";
-  auto x1 = (-b + sqrt(D)) / (2 * a);
-  auto x2 = (-b - sqrt(D)) / (2 * a);
-  std::cout << "x1=" << x1 << "\n";
-  std::cout << "x2=" << x2 << "\n";
+int main(int argc, char *argv[]) {
+  const int kMinNum = 2, kMaxNum = 255;
+  std::cout << "This programm calculates discriminant \n"
+               "ax^2+bx+c=0\n"
+               "D = b^2 - 4 * a * c\n";
+  int a = 1, b = 2, c = 3;
+  const char a_check = 97, b_check = 98, c_check = 99;
+  a = input_check(a, kMinNum, kMaxNum, a_check);
+  b = input_check(b, kMinNum, kMaxNum, b_check);
+  c = input_check(a, kMinNum, kMaxNum, c_check);
+  double D{};
+  double x1{}, x2{};
+  Dis_check(D, a, b, c, x1, x2);
   return 0;
 }
