ccb1a3820728b6f8b0f509d974a1d7b0e7118d5a
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-12 18:57:18 +0200

    Update Maksim_Panibrat_HW2.3.cpp

--- HomeWork2/Maksim_Panibrat_HW2.3.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.3.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
-int main() {
-  std::cout << "This program turns number in reverse.\n";
-  std::cout << "Enter number to reverse it\n";
-  int number;
-  std::cin >> number;
-  int reverse_number{0};
-  if (number < 10) {
-    std::cout << "There is no need to reverse.\n Reverse number: " << number
-              << std::endl;
-    return 0;
-  }
-  do {
-    reverse_number *= 10;
-    reverse_number += number % 10;
-    number /= 10;
-  } while (number != 0);
-
-  std::cout << "Reverse number: " << reverse_number << "\n";
-  return 0;
+void reverse_num(int number) {
+    int reverse_num{};
+    while (number != 0){
+        reverse_num *= 10;
+        reverse_num += number % 10;
+        number /= 10;
+    }
+    std::cout << "Result: " << reverse_num << std::endl;
 }
 
+
+int main(int argc, char* argv[]) {
+    std::cout << "This program turns number in reverse.\n"
+    "Enter number to reverse it\n";
+    int number;
+    std::cin >> number;
+    reverse_num(number);
+
+    return 0;
+}
