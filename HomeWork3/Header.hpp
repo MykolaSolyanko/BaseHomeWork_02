4a5b24eb890a2718040ba873b6ef60c9133a4458
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-03-12 23:32:46 +0200

    Header.hpp

---
+++ HomeWork3/Header.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+char* cin_char_array(int kSize);
+int sizeof_char_array(char* array);
+void PrintCharArrayReverse(char* array, int size);
+void PrintUpperCaseCharArray(char* array, int size);
+void PrintLowerCaseCharArray(char* array, int size);
+void is_digit(char* array, int size);
+
+void PartialSortElemArray(int* array, int size);
+void PrintIntArray(int* array, int size);
