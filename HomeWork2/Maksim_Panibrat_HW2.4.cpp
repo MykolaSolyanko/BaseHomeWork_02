7f354b906f78d794f55d475b34e6c2e0d80fa83d
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-12 19:09:52 +0200

    Update Maksim_Panibrat_HW2.4.cpp

--- HomeWork2/Maksim_Panibrat_HW2.4.cpp
+++ HomeWork2/Maksim_Panibrat_HW2.4.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 
-int main() {
+
+int main(int argc, char* argv[]) {
     std::cout << "This programm sums up even numbers.\n Uneven numbers will be "
-        "discarded.\n";
-    const int kMinNumber = 1, kMaxNumber = 50;
-    std::cout << "Enter amount of numbers in range [" << kMinNumber << "..."
-        << kMaxNumber << "]\n";
+        "discarded.\n"
+    "Enter number of values: \n";
     int number;
     std::cin >> number;
-    while (number < kMinNumber || number > kMaxNumber) {
-        std::cout << "Error, out of amount range\n";
-        std::cin >> number;
-    }
-    const int kMinValue = -60, kMaxValue = 90;
-    std::cout << "Enter numbers in range [" << kMinValue << "..." << kMaxValue
-        << "]\n";
-    size_t sum{ 0 }, iter{ 1 };
-    while (iter <= number) {
+    int sum{}, i = 1;
+    while (number != 0) {
         int value;
+        std::cout << "Enter value"<< i<<": ";
         std::cin >> value;
-        if (value < kMinValue || value > kMaxValue) {
-            std::cout << "Error, out of value range, please try again\n";
-            continue;
-        }
-        if (value % 2) {
+        if (value % 2 != 0) {
             sum += value;
         }
-        ++iter;
+        --number;
+        i++;
     }
-    std::cout << "Sum of even numbers = " << sum << std::endl;
+    std::cout << "Sum of even numbers: " << sum << std::endl;
     return 0;
 }
