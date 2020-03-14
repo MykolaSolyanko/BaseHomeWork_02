afe7f6b37c49882c73fd4247daad5c704fdd2e60
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-03-14 20:26:04 +0200

    updated

--- HomeWork3/Source.cpp
+++ HomeWork3/Source.cpp
@@ -92,7 +92,7 @@ void PartialSortElemArray(int* array, int size) {
         for (int j = 0; j < size - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
                 tmp = array[j];
-                array[j] = array[i + 1];
+                array[j] = array[j + 1];
                 array[j + 1] = tmp;
             }
         }
