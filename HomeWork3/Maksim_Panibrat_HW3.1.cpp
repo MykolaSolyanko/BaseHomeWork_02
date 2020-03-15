8fb475ccfbf319a75b27c4cbad11c33d1c28c22c
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-15 16:46:06 +0200

    Update Maksim_Panibrat_HW3.1.cpp

--- HomeWork3/Maksim_Panibrat_HW3.1.cpp
+++ HomeWork3/Maksim_Panibrat_HW3.1.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 
-void Reverse(char *array, const size_t kMaxInput) {
-  size_t i = 1;
-  while (i <= kMaxInput) {
-    if (array[kMaxInput - i] != 0) {
-      std::cout << array[kMaxInput - i];
-    }
-    i++;
-  }
-}
 
-int main(int argc, char *argv[]) {
-  std::cout << "This program reverses string\n";
-  const size_t kMaxInput = 100;
-  char array[kMaxInput]{};
-  std::cout << "Enter string for reverse, in range of " << kMaxInput
-            << " symbols:\n";
-  std::cin >> array;
-  Reverse(array, kMaxInput);
-  return 0;
+int main(int argc, char* argv[]) {
+    std::cout << "This program reverses string\n";
+    const size_t kMaxSize = 11;
+    char array[kMaxSize]{};
+    std::cout << "Enter string for reverse, in range of " << kMaxSize - 1 << " symbols: ";
+    std::cin >> array;
+    const int size = strlen(array);
+    int i{};
+    char* begin = { array - 1 }, * end = { begin + size }, temp_array[kMaxSize]{};
+    while (*end!=*begin) {
+        temp_array[i] = *end;
+        --end;
+        i++;
+    }
+    std::cout << "Result: " << array    << std::endl;
+    return 0;
 }
