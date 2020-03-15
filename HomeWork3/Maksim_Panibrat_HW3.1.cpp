2a426625fa112b18205fcbe02eba0984aa0ad49e
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-15 16:47:47 +0200

    Update Maksim_Panibrat_HW3.1.cpp

--- HomeWork3/Maksim_Panibrat_HW3.1.cpp
+++ HomeWork3/Maksim_Panibrat_HW3.1.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "This program reverses string\n";
-    const size_t kMaxSize = 11;
+    const size_t kMaxSize = 31;
     char array[kMaxSize]{};
     std::cout << "Enter string for reverse, in range of " << kMaxSize - 1 << " symbols: ";
     std::cin >> array;
