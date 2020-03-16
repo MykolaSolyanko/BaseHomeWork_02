59e4091aecd84011f8481e677828ef649e70520f
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-16 16:19:23 +0200

    Update Maksim_Panibrat_HW3.1.cpp

--- HomeWork3/Maksim_Panibrat_HW3.1.cpp
+++ HomeWork3/Maksim_Panibrat_HW3.1.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 
-
-int main(int argc, char* argv[]) {
-    std::cout << "This program reverses string\n";
-    const size_t kMaxSize = 31;
-    char array[kMaxSize]{};
-    std::cout << "Enter string for reverse, in range of " << kMaxSize - 1 << " symbols: ";
-    std::cin >> array;
-    const int size = strlen(array);
-    int i{};
-    char* begin = { array - 1 }, * end = { begin + size }, temp_array[kMaxSize]{};
-    while (*end!=*begin) {
-        temp_array[i] = *end;
-        --end;
-        i++;
-    }
-    i=0;
-    std::cout << "Result: ";
-        while (i != kMaxSize) {
-        array[i] = temp_array[i];
-        if (temp_array[i] != 0) {
-            std::cout << array[i];
-        }
-        i++;
-    }
-    return 0;
+int main(int argc, char *argv[]) {
+  std::cout << "This program reverses string\n";
+  const size_t kMaxSize = 31;
+  char array[kMaxSize]{};
+  std::cout << "Enter string for reverse, in range of " << kMaxSize - 1
+            << " symbols: ";
+  std::cin >> array;
+  const size_t size = strlen(array);
+  char *begin = {array - 1}, *end = {begin + size}, temp_array[kMaxSize]{},
+       *temp = temp_array, *arr = array;
+  while (*end != *begin) {
+    *temp = *end;
+    --end;
+    temp++;
+  }
+  temp = temp_array - 1;
+  while (array[size - 1] != temp_array[size - 1]) {
+    *arr = *(temp + 1);
+    temp++;
+    arr++;
+  }
+  std::cout << "Result: " << array;
+  return 0;
 }
