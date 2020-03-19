3f690fd2a6c3057341b2d5862583bfdeabc79bd1
SerghiiKhirniy <khirniy.s@gmail.com>
2020-03-19 06:42:21 +0200

    Update S.KhirniyHw2_Happy ticket.cpp

--- HomeWork2/S.KhirniyHw2_Happy ticket.cpp
+++ HomeWork2/S.KhirniyHw2_Happy ticket.cpp
@@ -3,23 +3,21 @@
 #include <iostream>
 
 int main() {
-  std::cout << "input 6 digit number of your ticket : " << std::endl;
-  int32_t number;
-  std::cin >> number;
-
-  if (number >= 100'00 && number <= 999'999) {
-    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
-    a = number % 10;
-    b = (number / 10) % 10;
-    c = (number / 100) % 10;
-    d = (number / 1000) % 10;
-    e = (number / 10000) % 10;
-    f = (number / 100000) % 10;
-    (a + b + c == e + d + f) ? std::cout << "Happy ticket!"
-                             : std::cout << "No happy(" << std::endl;
-  } else {
-    std::cout << "Input error, Please try again" << std::endl;
+  std::cout << "Input 6 digit number of your ticket " << std::endl;
+  int num;
+  std::cin >> num;
+  const int kMinNumber = 100000, kMaxNumber = 999999;
+  while (num < kMinNumber || num > kMaxNumber) {
+    std::cout << "Your ticket number is out of range\n";
+    std::cin >> num;
   }
-
+  int left_num = (num / 100000) + ((num / 10000) % 10) + ((num / 1000) % 10);
+  std::cout << "left sum = " << left_num << std::endl;
+  int right_num = ((num / 100) % 10) + ((num / 10) % 10) + ((num / 1) % 10);
+  std::cout << "right sum = " << right_num << std::endl;
+  std::cout << "You "
+            << (left_num == right_num ? "Ticket is HAPPY!!!"
+                                      : "The ticket is not happy( ...");
+  std::cout << std::endl;
   return 0;
 }
