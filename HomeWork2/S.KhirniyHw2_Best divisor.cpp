a2a2420d80ee7484ab6b017deb2cd3ebc5d3c303
SerghiiKhirniy <khirniy.s@gmail.com>
2020-03-19 06:22:32 +0200

    Update S.KhirniyHw2_Best divisor.cpp

--- HomeWork2/S.KhirniyHw2_Best divisor.cpp
+++ HomeWork2/S.KhirniyHw2_Best divisor.cpp
@@ -8,13 +8,24 @@ int main() {
 
   int n;
   std::cin >> n;
-
-  int i = n / 2;
-  while (n % i != 0)
-    i--;
-  if (i != 1)
-    std::cout << "The best divisor is -  " << i << std::endl;
-  else
-    std::cout << "There is no better divisor \n";
-  return 0;
+  if (n < 0) {
+    n = -n;
+  }
+  int best_sum{}, best_divisor{}, divisor{n};
+  while (divisor != 0) {
+    if (n % divisor == 0) {
+      int sum{}, temp_divisor{divisor};
+      while (temp_divisor != 0) {
+        sum += temp_divisor % 10;
+        temp_divisor /= 10;
+      }
+      if (sum > best_sum) {
+        best_divisor = divisor;
+        best_sum = sum;
+      }
+      std::cout << "Divisor: " << divisor << std::endl;
+    }
+    --divisor;
+  }
+  std::cout << "Best divisor = " << best_divisor << std::endl;
 }
