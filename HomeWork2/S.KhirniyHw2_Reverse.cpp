34d26aec99d4559a591e9deba2462409e1e15f47
SerghiiKhirniy <khirniy.s@gmail.com>
2020-03-19 07:00:57 +0200

    Update S.KhirniyHw2_Reverse.cpp

--- HomeWork2/S.KhirniyHw2_Reverse.cpp
+++ HomeWork2/S.KhirniyHw2_Reverse.cpp
@@ -4,14 +4,19 @@
 
 int main() {
 
-  std::cout << "Enter digit to reverse: " << std::endl;
-  int32_t s;
-  std::cin >> s;
-  while (s) {
-    std::cout << s % 10;
-    s /= 10;
+  std::cout << "Enter number to reverse: \n";
+  int32_t number;
+  std::cin >> number;
+  int reverse_number{0};
+  if (number < 10) {
+    std::cout << "This number is no reverse" << std::endl;
+    return 0;
   }
-  std::cout << std::endl;
-
+  do {
+    reverse_number *= 10;
+    reverse_number += number % 10;
+    number /= 10;
+  } while (number != 0);
+  std::cout << "Reverse number is: " << reverse_number << std::endl;
   return 0;
 }
