b50ce5774f3f11e970e8e1d438abc203ad3c790b
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-03-21 01:43:39 +0200

    updated

--- HomeWork3/Header.hpp
+++ HomeWork3/Header.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 char *alloc_char_array(size_t kSize);
+void fill_char_array(char *array, size_t kSize);
 size_t length_char_array(char *array);
 void CharArrayReverse(char *array, int size);
 void PrintCharArray(char *array, int size);
 void PrintUpperCaseCharArray(char *array, int size);
 void PrintLowerCaseCharArray(char *array, int size);
-void is_digit(char *array, int size);
+void print_is_digit(char *array, int size);
 
 void PartialSortElemArray(int *array, int size);
 void PrintIntArray(int *array, int size);
