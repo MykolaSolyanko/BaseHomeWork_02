c6bbb42e471b6561298c620dc93e216226f9ae35
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-03-22 23:58:28 +0200

    Updated

--- Homework4/Header.hpp
+++ Homework4/Header.hpp
@@ -1,4 +1,8 @@
+
 #pragma once
+using IntCompare = std::function<bool(int, int)>;
+using DoubleCompare = std::function<bool(double, double)>;
+using CharCompare = std::function<bool(char, char)>;
 char *AllocCharArray(size_t kSize);
 size_t LengthCharArray(char *array);
 void CreateRandArray(char *array, const int size);
@@ -9,26 +13,29 @@ void CreateCustomArray(double *array, const int size);
 void PrintArray(int *array, const int size);
 void PrintArray(double *array, const int size);
 void PrintArray(char *array, const int size);
-void swap(int *a, int *b);
-void swap(double *a, double *b);
-void swap(char *a, char *b);
-// std::function<bool(int, int)>  more(int a, int b);
-// std::function<bool(double, double)> more(double a, double b);
-// std::function<bool(char, char)> more(char a, char b);
-// std::function<bool(int, int)> less(int a, int b);
-// std::function<bool(double, double)> less(double a, double b);
-// std::function<bool(char, char)>  less(char a, char b);
-void BubbleSort(int *array, int size, std::function<bool(int, int)> compare);
-void BubbleSort(double *array, int size,
-                std::function<bool(double, double)> compare);
-void BubbleSort(char *array, int size, std::function<bool(char, char)> compare);
-void QuickSort(int *array, int low, int high,
-               std::function<bool(int, int)> compare);
-void QuickSort(double *array, int low, int high,
-               std::function<bool(double, double)> compare);
-void QuickSort(char *array, int low, int high,
-               std::function<bool(char, char)> compare);
-void QuickSortTime(int *array, int low, int high,
-                   std::function<bool(int, int)> compare);
-void QuickSortTime(double *array, int low, int high,
-                   std::function<bool(double, double)> compare);
+void swap(int &a, int &b);
+void swap(double &a, double &b);
+void swap(char &a, char &b);
+bool more(int a, int b);
+bool more(double a, double b);
+bool more(char a, char b);
+bool less(int a, int b);
+bool less(double a, double b);
+bool less(char a, char b);
+void BubbleSort(int *array, int size, IntCompare compare);
+void BubbleSort(double *array, int size, DoubleCompare compare);
+void BubbleSort(char *array, int size, CharCompare compare);
+void BubbleSortTime(int *array, int size, IntCompare compare);
+void BubbleSortTime(double *array, int size, DoubleCompare compare);
+void BubbleSortTime(char *array, int size, CharCompare compare);
+void QuickSort(int *array, int low, int high, IntCompare compare);
+void QuickSort(double *array, int low, int high, DoubleCompare compare);
+void QuickSort(char *array, int low, int high, CharCompare compare);
+void QuickSortTime(int *array, int low, int high, IntCompare compare);
+void QuickSortTime(double *array, int low, int high, DoubleCompare compare);
+void CreateIntArray(int *array, int size);
+void SortArray(int *array, int size);
+void CreateDoubleArray(double *array, int size);
+void SortDoubleArray(double *array, int size);
+std::pair<char *, int> CreateCharArray(char *array);
+void SortCharArray(char *array, int size);
