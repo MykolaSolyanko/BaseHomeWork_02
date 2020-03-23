87699e95c84ff3328cf4f7bec054d67b7eb4c420
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-23 19:46:51 +0200

    Update Alex_Zubok_home_work2_1.cpp

--- HomeWork2/Alex_Zubok_home_work2_1.cpp
+++ HomeWork2/Alex_Zubok_home_work2_1.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-int main() 
-{
-    unsigned long long var {}, summ_var {};
-    std::cout << "Please enter integer value:";
-    std::cin >> var;
-    size_t index{};
-    const uint8_t kZero = 0;
-    const uint8_t kTen = 10;
-    for ( size_t tmp = var, summ {}; tmp > kZero; tmp /= kTen ) {
-        summ += tmp % kTen;
-        ++index;
-        summ_var = summ;
-    }
-    std::cout << "Summ of ";
-    for ( size_t tmp = var, i = index; i > kZero; i-- ){
-        int tmp_num = tmp / pow( kTen, i - 1 );
-        std::cout << tmp_num;
-        tmp -= tmp_num * pow( kTen, i - 1 );
-        std::cout << ( i < 2 : " = " : " + " );
-    }
-    std::cout << summ_var << std::endl;
-    std::cout << "Arithmetic means of " << summ_var << " / " << index << " = " << ( summ_var / index ) << std::endl;
-    return 0;
+int main() {
+  unsigned long long var{}, summ_var{};
+  std::cout << "Please enter integer value:";
+  std::cin >> var;
+  size_t index{};
+  const uint8_t kZero = 0;
+  const uint8_t kTen = 10;
+  for (size_t tmp = var, summ{}; tmp > kZero; tmp /= kTen) {
+    summ += tmp % kTen;
+    ++index;
+    summ_var = summ;
+  }
+  std::cout << "Summ of ";
+  for (size_t tmp = var, i = index; i > kZero; i--) {
+    int tmp_num = tmp / pow(kTen, i - 1);
+    std::cout << tmp_num;
+    tmp -= tmp_num * pow(kTen, i - 1);
+    std::cout << (i < 2 : " = " : " + ");
+  }
+  std::cout << summ_var << std::endl;
+  std::cout << "Arithmetic means of " << summ_var << " / " << index << " = " << (summ_var / index) << std::endl;
+  return 0;
 }
