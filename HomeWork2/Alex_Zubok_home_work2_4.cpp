98e4b8f05eddb2f0ca5584f735358b6e2ee05476
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-23 15:35:09 +0200

    Update Alex_Zubok_home_work2_4.cpp

--- HomeWork2/Alex_Zubok_home_work2_4.cpp
+++ HomeWork2/Alex_Zubok_home_work2_4.cpp
@@ -5,7 +5,7 @@ bool range_check( long long x, int a, int b ){
 }
 
 bool modul_remainder( int value, unsigned int modul_value ){
-    return ( value % modul_value ) > 0;
+    return ( value % modul_value ) != 0;
 }
 
 void check_valid_input( int &value, int a, int b ){
@@ -22,12 +22,12 @@ int main(){
     const int kMax_inputs = 90, kMax_range = 50;
     std::cout << "Please enter number of quantity inputs number from 1 till 50: ";
     check_valid_input( iter, kMin_range, kMax_range );
-    std::cout << "Please enter " << iter << " inputs[-60 - 90]." << std::endl;
+    std::cout << "Please enter " << iter << " inputs[ " << kMin_inputs << " - " << kMax_inputs << " ]." << std::endl;
     long long summ{};
     const int kTwo = 2;
-    for ( size_t index = 0; index < iter; index++){
+    for ( size_t index = 1; index <= iter; index++){
         int tmp {};
-        std::cout << "Enter " << ( index + 1 ) << " value: ";
+        std::cout << "Enter " << index << " value: ";
         check_valid_input( tmp, kMin_inputs, kMax_inputs );
         if ( modul_remainder(tmp, kTwo) )
             summ += tmp;
