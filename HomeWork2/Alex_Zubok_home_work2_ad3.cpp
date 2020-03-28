57ce5d148acddc7f51e47c03bda80159b393ea74
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-28 16:05:12 +0200

    Update Alex_Zubok_home_work2_ad3.cpp

--- HomeWork2/Alex_Zubok_home_work2_ad3.cpp
+++ HomeWork2/Alex_Zubok_home_work2_ad3.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <bitset>
 
-int main()
-{
-	unsigned long value {}, number {};
-  const int kOne = 1;
-	std::cout << "Please enter number from 0 till 255: ";
-	std::cin >> value >> number;
-	std::bitset<8> one_byte(value);
+void bite_check(uint16_t byte_arr[8], size_t index) {
+  std::cout << ((byte_arr[8 - index]) ? "Yes" : "No") << std::endl;
+}
 
-	std::cout << ( ( one_byte.test( number - kOne ) )? "Yes":"No" ) << std::endl;
-	return 0;
+int main() {
+  unsigned long value{}, number{};
+  uint16_t one_byte[8]{};
+  std::cout << "Please enter a number from 0 till 255 and number of the bit that you want a check for set: ";
+  std::cin >> value >> number;
+  const uint8_t kByte = 7;
+
+  for (uint16_t tmp = value, index = 0; tmp > 0; tmp /= 2, index++)
+    one_byte[kByte - index] = tmp % 2;
+
+  bite_check(one_byte, number);
+
+  return 0;
 }
