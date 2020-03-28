0dff20898abc61f8a016350471224134d479bc93
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-28 13:27:40 +0200

    Update Alex_Zubok_home_work2_1.cpp

--- HomeWork2/Alex_Zubok_home_work2_1.cpp
+++ HomeWork2/Alex_Zubok_home_work2_1.cpp
@@ -1,10 +1,23 @@
 #include <cmath>
 #include <iostream>
 
+void check_valid_input(long long &value) {
+  while (!(std::cin >> value) || value < std::numeric_limits<unsigned int>::min() || value > std::numeric_limits<unsigned int>::max()) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Sorry, you enter not positive integer value, please enter again: ";
+  }
+}
+
 int main() {
-  unsigned long long var{}, summ_var{};
-  std::cout << "Please enter integer value:";
-  std::cin >> var;
+  long long var{}, summ_var{};
+  std::cout << "Please enter a positive integer value:";
+  check_valid_input(var);
+  if (var == 0) {
+    std::cout << "Summ of 1 element is 0" << std::endl;
+    std::cout << "Arithmetic means of 0 is 0" << std::endl;
+    return 0;
+  }
   size_t index{};
   const uint8_t kZero = 0;
   const uint8_t kTen = 10;
@@ -18,7 +31,7 @@ int main() {
     int tmp_num = tmp / pow(kTen, i - 1);
     std::cout << tmp_num;
     tmp -= tmp_num * pow(kTen, i - 1);
-    std::cout << (i < 2 : " = " : " + ");
+    std::cout << (i < 2 ? " = " : " + ");
   }
   std::cout << summ_var << std::endl;
   std::cout << "Arithmetic means of " << summ_var << " / " << index << " = " << (summ_var / index) << std::endl;
