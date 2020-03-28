0e43c57d4de0c737adfcfb40b182297b82b5d3b1
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-28 14:13:06 +0200

    Update Alex_Zubok_home_work2_5.cpp

--- HomeWork2/Alex_Zubok_home_work2_5.cpp
+++ HomeWork2/Alex_Zubok_home_work2_5.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 
-bool is_delimiter(int value, int value_delim){
-    return ( value % value_delim ) == 0;
+bool is_delimiter(int value, int value_delim) {
+  return (value % value_delim) == 0;
 }
-int summ_number( int value ){
-    int summ{};
-    const uint8_t kTen = 10;
-    for ( int tmp = value; tmp > 0; tmp /= kTen )
-        summ += ( tmp % kTen );
-    return summ;
+int summ_number(int value) {
+  int summ{};
+  const uint8_t kTen = 10;
+  for (int tmp = value; tmp > 0; tmp /= kTen)
+    summ += (tmp % kTen);
+  return summ;
 }
 
-int main(){
-    unsigned int value{};
-    std::cin >> value;
-    for ( int i = value; i > 0; i-- ) {
-        if ( is_delimiter( value, i ) ) {
-            for ( int j = i - 1; j > 0; j-- ){
-                if ( is_delimiter( value, j ) ) {
-                    if ( summ_number( i ) > summ_number( j ) ) {
-                        std::cout << "Best delimiter is " << i << std::endl;
-                        return 0;
-                    }
-                    else {
-                        std::cout << "Best delimiter is " << j << std::endl;
-                        return 0;
-                    }
-                }
-            }
+void check_valid_input(long long &value) {
+  while (!(std::cin >> value) || value < std::numeric_limits<unsigned int>::min() || value > std::numeric_limits<unsigned int>::max()) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Sorry, you enter not positive integer value, please enter again: ";
+  }
+}
+
+int main() {
+  unsigned int value{};
+  check_valid_input(value);
+  unsigned int best_delimeter = value;
+  for (int i = value; i > 0; i--) {
+    if (is_delimiter(value, i)) {
+      for (int j = i - 1; j > 0; j--) {
+        if (is_delimiter(value, j)) {
+          if (summ_number(best_delimeter) < summ_number(j)) {
+            best_delimeter = j;
+          }
         }
+      }
     }
+  }
+  std::cout << "The best delimeter for " << value << " is a number " << best_delimeter << std::endl;
+  return 0;
 }
