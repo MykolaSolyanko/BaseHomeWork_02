4ac4026cbba5cc78e9a059cf74e30699a17f4b83
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-28 15:44:01 +0200

    Update Alex_Zubok_home_work2_ad2.cpp

--- HomeWork2/Alex_Zubok_home_work2_ad2.cpp
+++ HomeWork2/Alex_Zubok_home_work2_ad2.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-#include <bitset>
 
-int main()
-{
-	unsigned long value {};
-	std::cout << "Please enter number from 0 till 255: ";
-	std::cin >> value;
-	std::bitset<8> one_byte(value);
-	std::cout << "Integer number is " << value << " ---> " << one_byte << " > number of bites set is " << one_byte.count() << std::endl;
-	return 0;
+void print_byte(uint16_t byte_arr[8]) {
+  for (size_t index = 0; index < 8; index++)
+    std::cout << byte_arr[index];
+}
+size_t count_byte(uint16_t byte_arr[8]) {
+  size_t count{};
+  for (size_t index = 0; index < 8; index++) {
+    if (byte_arr[index] == 1)
+      ++count;
+  }
+  return count;
+}
+
+int main() {
+  unsigned long value{};
+  uint16_t one_byte[8]{};
+  std::cout << "Please enter number from 0 till 255: ";
+  std::cin >> value;
+  const uint8_t kByte = 7;
+
+  for (uint16_t tmp = value, index = 0; tmp > 0; tmp /= 2, index++)
+    one_byte[kByte - index] = tmp % 2;
+
+  std::cout << value << " ---> ";
+  print_byte(one_byte);
+  std::cout << " set bites is " << count_byte(one_byte) << std::endl;
+
+  return 0;
 }
