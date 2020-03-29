b388f08d11dc128e865d9e90fdb83cf9d6fce182
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-29 17:18:24 +0300

    Update Alex_Zubok_home_work2_ad3.cpp

--- HomeWork2/Alex_Zubok_home_work2_ad3.cpp
+++ HomeWork2/Alex_Zubok_home_work2_ad3.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 
-void bite_check(uint16_t byte_arr[8], size_t index) {
-  std::cout << ((byte_arr[8 - index]) ? "Yes" : "No") << std::endl;
-}
-
 int main() {
-  unsigned long value{}, number{};
-  uint16_t one_byte[8]{};
-  std::cout << "Please enter a number from 0 till 255 and number of the bit that you want a check for set: ";
-  std::cin >> value >> number;
-  const uint8_t kByte = 7;
-
-  for (uint16_t tmp = value, index = 0; tmp > 0; tmp /= 2, index++)
-    one_byte[kByte - index] = tmp % 2;
-
-  bite_check(one_byte, number);
-
+  size_t value{}, check_bit{};
+  std::cout << "Please enter value from 1 till 2550000 and checking bit: ";
+  std::cin >> value >> check_bit;
+  std::cout << ((value & (1 << (check_bit - 1))) ? "Yes" : "No") << std::endl;
   return 0;
 }
