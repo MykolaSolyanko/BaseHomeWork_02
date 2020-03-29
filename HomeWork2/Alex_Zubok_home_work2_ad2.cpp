0d4e5a2317c4b46a21682f927cc6605bd30e447f
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-29 17:11:06 +0300

    Update Alex_Zubok_home_work2_ad2.cpp

--- HomeWork2/Alex_Zubok_home_work2_ad2.cpp
+++ HomeWork2/Alex_Zubok_home_work2_ad2.cpp
@@ -1,9 +1,10 @@
+#include <bitset>
 #include <iostream>
 
-size_t count_bit(size_t value){
+size_t count_bit(size_t value) {
   size_t count{};
-  for (size_t i = 0, tmp = value; tmp > 0; i++, tmp /= 2){
-    if ( value & (1 << i))
+  for (size_t i = 0, tmp = value; tmp > 0; i++, tmp /= 2) {
+    if (value & (1 << i))
       ++count;
   }
   return count;
@@ -13,6 +14,7 @@ int main() {
   size_t value{};
   std::cout << "Please enter value from 1 till 2550000: ";
   std::cin >> value;
-  std::cout << value << " bit counts is : " << count_bit(value) << std::endl;
+  std::bitset<24> bits{value};
+  std::cout << value << " ---> " << bits << " > set bits is " << count_bit(value) << std::endl;
   return 0;
 }
