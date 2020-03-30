922c50ff08a4d22c05ddf6d635de890e8f09dc44
Sam4uk <sto.ukr.net@gmail.com>
2020-03-30 23:18:00 +0300

    UpLOAD

--- Homework5/Taras_Samchuk_HW5.cpp
+++ Homework5/Taras_Samchuk_HW5.cpp
@@ -2,9 +2,10 @@
 
 #include "Classic_TS_HW5.hpp"
 int main(int argc, char const *argv[]) {
-  node_one_ptr *tmp{nullptr};
+  node_one_ptr *finder{nullptr};
   node_one_ptr *list{nullptr};
-  // print_list(list);
+  // show empty list
+  print_list(list);
   push_front(list, 1);
   push_front(list, 2);
   push_front(list, 3);
@@ -13,15 +14,17 @@ int main(int argc, char const *argv[]) {
   push_front(list, 6);
   print_list(list);
   push_back(list, 7);
-  tmp = find(list, 4);
-  insert(list, tmp, 55);
+
+  finder = find(list, 4);
+  insert(list, finder, 55);
+
   print_list(list);
   pop_front(list);
-  // pop_back(list);
+  pop_back(list);
   print_list(list);
-  std::cout << (find(list, 4)) << std::endl;
 
-  // std::cout << viewfront(list) << std::endl;
-  // std::cout << viewback(list) << std::endl;
+  std::cout << viewfront(list) << std::endl;
+  std::cout << viewback(list) << std::endl;
+  clear_list(list);
   return 0;
 }
