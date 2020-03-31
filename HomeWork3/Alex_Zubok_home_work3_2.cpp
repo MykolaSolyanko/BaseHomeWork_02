b0887dd3675002f9874abf99669f57326a843d71
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-03-31 15:43:10 +0300

    Update Alex_Zubok_home_work3_2.cpp

--- HomeWork3/Alex_Zubok_home_work3_2.cpp
+++ HomeWork3/Alex_Zubok_home_work3_2.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
-void sort_array(int *array, size_t index) {
-  for (size_t i = 0; i < index; i++) {
-    for (size_t j = i + 1; j < index + 1; j++) {
-      if (*(array + j) < *(array + i)) {
-        int tmp = *(array + i);
-        *(array + i) = *(array + j);
-        *(array + j) = tmp;
+void sort_array(int *begin, const int *const end) {
+  if (begin == nullptr || end == nullptr || begin == end)
+    return;
+  for (int *i = begin; i != end; i++) {
+    for (int *j = i + 1; j != end + 1; j++) {
+      if (*j < *i) {
+        int tmp = *i;
+        *i = *j;
+        *j = tmp;
       }
     }
   }
 }
 
-void print_array(const int *array, size_t index) {
-  std::cout << "Array[ " << *array;
-  for (size_t i = 1; i < index; i++)
-    std::cout << ", " << *(array + i);
+void print_array(const int *begin, const int *const end) {
+  std::cout << "Array[ " << *begin;
+  for (const int *i = begin + 1; i != end; i++)
+    std::cout << ", " << *i;
   std::cout << " ]" << std::endl;
 }
 
@@ -27,8 +29,8 @@ int main() {
   for (size_t i = 0; i < arr_size; i++) {
     std::cout << "Please enter the array element: ";
     std::cin >> *(sorted_array + i);
-    sort_array(sorted_array, i);
-    print_array(sorted_array, arr_size);
+    sort_array(sorted_array, sorted_array + i);
+    print_array(sorted_array, sorted_array + arr_size);
   }
   delete[] sorted_array;
   return 0;
