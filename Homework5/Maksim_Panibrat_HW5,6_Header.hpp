c193da2d1ef80b3f369ac0ee96f494d9ad81b6c3
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-31 20:44:46 +0300

    Create Maksim_Panibrat_HW5,6_Header.hpp

---
+++ Homework5/Maksim_Panibrat_HW5,6_Header.hpp
@@ -0,0 +1,44 @@
+#include "Header.h"
+
+int main() {
+  std::cout
+      << "This program realize one of classic data structures - Stack. \n";
+  Stack st;
+  std::cout << "Stack size: " << st.GetSizeStack() << std::endl;
+  int value;
+  {
+    Pair pair{true, ""};
+    while (pair.status == true) {
+      std::cin >> value;
+      pair = st.push(value);
+    }
+    std::cout << pair.msg << std::endl;
+  }
+  std::cout << "top " << st.top() << std::endl;
+  {
+    Pair pair{true, ""};
+    for (int i = 0; i != 6; i++) {
+      pair = st.pop();
+    }
+    std::cout << pair.msg << std::endl;
+  }
+  st.clear();
+  std::cout << "new top after clear: " << st.top() << std::endl;
+
+  {
+    Pair pair{true, ""};
+    for (int i = 0; i != 6; i++) {
+      std::cin >> value;
+      pair = st.push(value);
+    }
+    std::cout << pair.msg << std::endl;
+  }
+  std::cout << "top after iter push: " << st.top() << std::endl;
+
+  for (Pair pair{true, ""}; pair.status == true; pair = st.pop()) {
+  }
+
+  std::cout << "new top after popping " << st.top() << std::endl;
+
+  return 0;
+}
