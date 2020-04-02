512ed96b784bd567997cf3368e28ae35b91b3bb0
Sam4uk <sto.ukr.net@gmail.com>
2020-04-02 17:38:32 +0300

    TMP

---
+++ HomeWork7/Queue_TS_HW7Template.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include "Common_TS_HW7.hpp"
+// the queue is based on a bidirectional list
+
+template <typename T>
+struct node2directions {
+  T Inf{};
+  node2directions *next<T>{nullptr};
+  node2directions *prev<T>{nullptr};
+};
+
+template <typename T>
+class Queue {
+ public:
+  Queue(const size_t size);
+  Queue();
+  Queue(Queue &) = delete;
+  ~Queue();
+
+  bool enqueue(const T value);
+  result<T> peek();
+  result<T> dequeue();
+
+  void clear();
+
+  bool isEmpty() const;
+  bool isFull() const;
+
+  size_t getCount() const;
+  size_t getMaxSize() const;
+
+ private:
+  node2directions<T> *head{nullptr};
+  node2directions<T> *tail{nullptr};
+  size_t count{0};
+  const size_t k_max_size{k_defult_max_size};
+};
+
+Queue<T>::Queue(size_t size) : k_max_size{size} {}
+
+Queue<T>::Queue() {}
+
+Queue<T>::~Queue() {
+  if (count != 0) {
+    clear();
+  }
+}
+
+bool Queue<T>::enqueue(T value) {
+  if (count == k_max_size) {
+    return false;
+  }
+  node2directions<T> *last{nullptr};
+  if (tail != nullptr) {
+    last = tail;
+  }
+
+  node2directions<T> *NewComponent =
+      new node2directions<T>{value, nullptr, tail};
+  if (last != nullptr) {
+    last->next = NewComponent;
+  }
+  tail = NewComponent;
+  if (head == nullptr) {
+    head = NewComponent;
+  }
+  count++;
+  return true;
+}
+
+result<T> Queue<T>::peek() {
+  if (count == 0) {
+    return {{}, false};
+  }
+  return {head->Inf, true};
+}
+
+result<T> Queue<T>::dequeue() {
+  if (head == nullptr) {
+    return {{}, false};
+  }
+  node2directions<T> *to_delete = head;
+  node2directions<T> *hold = to_delete->next;
+  delete to_delete;
+  head = hold;
+  hold->prev = nullptr;
+  count--;
+  return {{hold->Inf}, true};
+}
+
+void Queue<T>::clear() {
+  if (count == 0) {
+    return;
+  }
+  node2directions<T> *arrow = head;
+  head = nullptr;
+  tail = nullptr;
+  count = 0;
+  while (arrow != tail) {
+    node2directions<T> *hold = arrow;
+    arrow = arrow->next;
+    delete hold;
+  }
+}
+
+bool Queue<T>::isEmpty() const { return count == 0; }
+
+bool Queue<T>::isFull() const { return count == k_max_size; }
+
+size_t Queue<T>::getCount() const { return count; }
+
+size_t Queue<T>::getMaxSize() const { return k_max_size; }
--- HomeWork7/Taras_Samchuk_HW7_Task_1.cpp
+++ HomeWork7/Taras_Samchuk_HW7_Task_1.cpp
@@ -1,11 +1,13 @@
-// #include <iostream>
+#include <iostream>
 
-#include <string>
-
-#include "Classic_TS_HW7.hpp"
+#include "Queue_TS_HW7Template.hpp"
+#include "StackOnArray_TS_HW7.hpp"
+// #include "Stack_TS_HW7.hpp" //Such a stack is also entitled to life
 int main(int argc, char const *argv[]) {
   Stack<short> Demo(11);
+  std::cout << Demo.top().first << std::endl;
   Demo.push(1);
+  std::cout << Demo.pop().first << std::endl;
   Demo.push(2);
   Demo.push(3);
   Demo.push(4);
@@ -13,17 +15,61 @@ int main(int argc, char const *argv[]) {
   Demo.push(6);
   Demo.push(7);
   Demo.push(8);
+  std::cout << Demo.pop().first << std::endl;
   Demo.push(9);
   Demo.push(10);
   Demo.push(11);
-  Demo.printAll();
-  Demo.swap();
-  Demo.printAll();
+  Demo.pop();
+  Demo.pop();
+  Demo.pop();
   Demo.clear();
+  /*
+  #include <iostream>
 
+  #include "Queue_TS_HW7.hpp"
+  #include "StackOnArray_TS_HW7.hpp"
+  // #include "Stack_TS_HW7.hpp" //Such a stack is also entitled to life
+  int main(int argc, char const *argv[]) {
+    Stack Demo;
+    std::cout << Demo.top().first << std::endl;
+    Demo.push(1);
+    std::cout << Demo.pop().first << std::endl;
+    Demo.push(2);
+    Demo.push(3);
+    Demo.push(4);
+    Demo.push(5);
+    Demo.push(6);
+    Demo.push(7);
+    Demo.push(8);
+    std::cout << Demo.pop().first << std::endl;
+    Demo.push(9);
+    Demo.push(10);
+    Demo.push(11);
+    Demo.pop();
+    Demo.pop();
+    Demo.pop();
+    Demo.clear();
+
+    Queue Demo2;
+    Demo2.enqueue(1.1);
+    Demo2.enqueue(2.2);
+    Demo2.enqueue(3.3);
+    Demo2.enqueue(4.4);
+    Demo2.enqueue(5.5);
+    Demo2.enqueue(6.6);
+    Demo2.enqueue(7.7);
+    Demo2.enqueue(8.8);
+    Demo2.enqueue(9.9);
+    Demo2.enqueue(10.01);
+    Demo2.enqueue(11.11);
+    Demo2.dequeue();
+    Demo2.dequeue();
+    Demo2.dequeue();
+    Demo2.clear();
+    return 0;
+  }
+  */
   Equeue<double> Demo2;
-  Demo2.printAllBackward();
-  Demo2.printAllForward();
   Demo2.enqueue(1.1);
   Demo2.enqueue(2.2);
   Demo2.enqueue(3.3);
@@ -35,10 +81,6 @@ int main(int argc, char const *argv[]) {
   Demo2.enqueue(9.9);
   Demo2.enqueue(10.01);
   Demo2.enqueue(11.11);
-  Demo2.printAllBackward();
-  Demo2.printAllForward();
-  Demo2.swap();
-  Demo2.printAllBackward();
   Demo2.clear();
   Demo2.printAllForward();
 
