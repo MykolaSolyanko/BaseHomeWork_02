c2c13c1c6dc8ea4254b1ed7ff2dc6f4df84240d8
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-04 09:01:37 +0300

    Update Alex_Zubok_home_work3_ad2.cpp

--- HomeWork3/Alex_Zubok_home_work3_ad2.cpp
+++ HomeWork3/Alex_Zubok_home_work3_ad2.cpp
@@ -10,6 +10,14 @@ void upper_string(char *string) {
   std::cout << std::endl;
 }
 
+char *ret_upper_string(char *string) {
+  for (size_t i = 0; i < std::strlen(string); i++) {
+    if (*(string + i) > 96 && *(string + i) < 123)
+      *(string + i) -= 32;
+  }
+  return string;
+}
+
 int main() {
   std::cout << "Please enter string: ";
   char *tmp_string = new char;
@@ -20,6 +28,7 @@ int main() {
   std::cout << "Your string is: " << string << std::endl;
   std::cout << "Your upper string is: ";
   upper_string(string);
+  std::cout << "Your upper return string is " << ret_upper_string(string) << std::endl;
   delete[] string;
   return 0;
 }
