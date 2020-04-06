d6a4a176b1236c816a9fd9170e00af2a0f45d708
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-04-06 23:13:30 +0300

    Header.hpp updated v2

--- HomeWork7/Header.hpp
+++ HomeWork7/Header.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 template <typename T> void PrintArray(const T *array, const int size) {
   for (int i{}; i < size; ++i) {
     std::cout << array[i] << " ";
@@ -10,8 +11,6 @@ template <typename T> void swap(T &a, T &b) {
   a = b;
   b = t;
 }
-template <typename T> bool more(T a, T b) { return a > b; }
-template <typename T> bool less(T a, T b) { return a < b; }
 template <typename T1, typename T2>
 void BubbleSort(T1 *arrptr, int size, T2 comp) {
   for (int i = 0; i < size - 1; ++i) {
@@ -25,6 +24,35 @@ void BubbleSort(T1 *arrptr, int size, T2 comp) {
   }
 }
 
-void CreateRandArray(char *array, const int size);
-void CreateRandArray(int *array, const int size);
-void CreateRandArray(double *array, const int size);
+template <typename T = int> void CreateIntRandArray(T *array, const int size) {
+  std::random_device rd;
+  std::mt19937 mersenne{rd()};
+  std::uniform_int_distribution<> dist(0, size);
+  for (int i{}; i < size; ++i) {
+    *(array + i) = dist(mersenne);
+  };
+}
+template <typename T = double>
+void CreateDoubleRandArray(T *array, const int size) {
+  std::random_device rd;
+  std::mt19937 mersenne{rd()};
+  std::uniform_real_distribution<> dist(0, size);
+  for (int i{}; i < size; ++i) {
+    *(array + i) = dist(mersenne);
+  }
+}
+
+template <typename T = char>
+void CreateCharRandArray(T *array, const int size) {
+  std::random_device rd;
+  std::mt19937 mersenne{rd()};
+  // std::knuth_b gen1;
+  std::uniform_int_distribution<> dist('0', 'z');
+  int index{};
+  while (index < size) {
+    int rand = dist(mersenne);
+    if (((rand > '@') && (rand < '[')) || (rand > '`') || (rand < ':')) {
+      *(array + index++) = rand;
+    }
+  }
+}
