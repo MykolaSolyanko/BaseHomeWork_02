6fd0c952c49f03d3c14c488c748886327ede1397
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-04-08 16:55:04 +0300

    updated v3

--- HomeWork8/Header.hpp
+++ HomeWork8/Header.hpp
@@ -4,67 +4,65 @@ class Shape {
 public:
   Shape() { std::cout << __FUNCSIG__ << std::endl; };
   virtual ~Shape() { std::cout << __FUNCSIG__ << std::endl; };
-  virtual void draw() = 0;
-  virtual int calc_square() = 0;
+  virtual const void draw() = 0;
+  virtual double calc_square() = 0;
 
 private:
 };
 
 class Square : public Shape {
 public:
-  Square() : Square(1) { std::cout << __FUNCSIG__ << std::endl; };
+  Square() = default;
   Square(int a) : side{a} { std::cout << __FUNCSIG__ << std::endl; };
   virtual ~Square() { std::cout << __FUNCSIG__ << std::endl; };
   ;
-  void draw() override;
-  int calc_square() override;
-  int get_side();
+  const void draw() override;
+  double calc_square() override;
+  const int get_side();
   void set_side(int side);
 
 private:
-  int side;
+  int side{1};
 };
 
 class RightTriangle : public Shape {
 public:
-  RightTriangle() : RightTriangle(1, 1) {
-    std::cout << __FUNCSIG__ << std::endl;
-  };
+  RightTriangle() = default;
   RightTriangle(int a, int h) : side{a}, height{h} {
     std::cout << __FUNCSIG__ << std::endl;
   };
   virtual ~RightTriangle() { std::cout << __FUNCSIG__ << std::endl; };
   ;
-  void draw() override;
-  int calc_square() override;
-  int get_side();
-  int get_height();
+  const void draw() override;
+  double calc_square() override;
+  const int get_side();
+  const int get_height();
   void set_side(int side);
   void set_height(int height);
 
 private:
-  int side;
-  int height;
+  int side{1};
+  int height{1};
 };
 
 class Rectangle : public Shape {
 public:
-  Rectangle() : Rectangle(1, 1) { std::cout << __FUNCSIG__ << std::endl; };
+  Rectangle() = default;
   Rectangle(int a, int h) : width{a}, height{h} {
     std::cout << __FUNCSIG__ << std::endl;
   };
   virtual ~Rectangle() { std::cout << __FUNCSIG__ << std::endl; };
   ;
-  void draw() override;
-  int calc_square() override;
-  int get_width();
-  int get_height();
+  const void draw() override;
+  double calc_square() override;
+  const int get_width();
+  const int get_height();
   void set_width(int width);
   void set_height(int height);
 
 private:
-  int width;
-  int height;
+  int width{1};
+  int height{1};
 };
 
 Square CreateSquare();
