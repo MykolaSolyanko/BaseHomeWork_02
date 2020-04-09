bb7d1b609adfbfd3b55151830dcb919b0883ec88
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-09 19:04:16 +0300

    Update Alex_Zubok_home_work4_2_sorts.cpp

--- Homework4/Alex_Zubok_home_work4_2_sorts.cpp
+++ Homework4/Alex_Zubok_home_work4_2_sorts.cpp
@@ -1,9 +1,14 @@
 #include <cstring>
 #include <iostream>
 
-using Comp_unsigned = bool (*)(unsigned, unsigned);
+void swap(unsigned *a, unsigned *b) {
+  unsigned tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
 bool More(unsigned a, unsigned b) { return a > b; }
-bool Less(unsigned a, unsigned b) { return a < b; }
+using Comp_unsigned = bool (*)(unsigned, unsigned);
 
 unsigned *user_partiotion(unsigned *begin, unsigned *end,
                           Comp_unsigned func = More) {
@@ -12,14 +17,10 @@ unsigned *user_partiotion(unsigned *begin, unsigned *end,
   for (unsigned *j = begin; j != end; j++) {
     if (func(pivot, *j)) { //*j < pivot !func
       i++;
-      unsigned tmp = *j;
-      *j = *i;
-      *i = tmp;
+      swap(i, j);
     }
   }
-  unsigned tmp = *(i + 1);
-  *(i + 1) = *(end - 1);
-  *(end - 1) = tmp;
+  swap(i + 1, end - 1);
   return (i + 1);
 }
 void qSort(unsigned *begin, unsigned *end, Comp_unsigned func = More) {
@@ -39,7 +40,12 @@ void print_array(const unsigned *begin, const unsigned *const end) {
 
 using Comp_double = bool (*)(double, double);
 bool More(double a, double b) { return a > b; }
-bool Less(double a, double b) { return a < b; }
+
+void swap(double *a, double *b) {
+  double tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
 
 double *user_partiotion(double *begin, double *end, Comp_double func = More) {
   double pivot = *(end - 1);
@@ -47,14 +53,10 @@ double *user_partiotion(double *begin, double *end, Comp_double func = More) {
   for (double *j = begin; j != end; j++) {
     if (func(pivot, *j)) { //*j < pivot !func
       i++;
-      double tmp = *j;
-      *j = *i;
-      *i = tmp;
+      swap(i, j);
     }
   }
-  double tmp = *(i + 1);
-  *(i + 1) = *(end - 1);
-  *(end - 1) = tmp;
+  swap(i + 1, end - 1);
   return (i + 1);
 }
 void qSort(double *begin, double *end, Comp_double func = More) {
@@ -74,7 +76,12 @@ void print_array(const double *begin, const double *const end) {
 
 using Comp_char = bool (*)(char, char);
 bool More(char a, char b) { return a > b; }
-bool Less(char a, char b) { return a < b; }
+
+void swap(char *a, char *b) {
+  char tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
 
 void print_array(const char *begin, const char *const end) {
   std::cout << "Array[ " << *begin;
@@ -86,11 +93,8 @@ void print_array(const char *begin, const char *const end) {
 void Sort(char *begin, const char *const end, Comp_char func = More) {
   for (char *c_l1 = begin; c_l1 != end; c_l1++) {
     for (char *c_l2 = c_l1 + 1; c_l2 != end; c_l2++) {
-      if (!func(*c_l2, *c_l1)) {
-        int tmp = *c_l1;
-        *c_l1 = *c_l2;
-        *c_l2 = tmp;
-      }
+      if (!func(*c_l2, *c_l1))
+        swap(c_l1, c_l2);
     }
   }
 }
@@ -105,11 +109,11 @@ void bSort(char *begin, const char *const end, Comp_char func = More) {
   size_t size_arr3{};
   size_t size_arr4{};
   for (char *c = begin; c != end; c++) {
-    if (*c < 91 && *c > 64)
+    if (*c <= 'Z' && *c >= 'A')
       size_arr1++;
-    else if (*c < 123 && *c > 96)
+    else if (*c <= 'z' && *c >= 'a')
       size_arr2++;
-    else if (*c < 58 && *c > 47)
+    else if (*c <= '9' && *c >= '0')
       size_arr3++;
     else
       size_arr4++;
@@ -120,13 +124,13 @@ void bSort(char *begin, const char *const end, Comp_char func = More) {
   array4 = new char[size_arr2];
   size_t i{}, j{}, k{}, l{};
   for (char *c = begin; c != end; c++) {
-    if (*c < 91 && *c > 64) {
+    if (*c <= 'Z' && *c >= 'A') {
       *(array1 + i) = *c;
       i++;
-    } else if (*c < 123 && *c > 96) {
+    } else if (*c <= 'z' && *c >= 'a') {
       *(array2 + j) = *c;
       j++;
-    } else if (*c < 58 && *c > 47) {
+    } else if (*c <= '9' && *c >= '0') {
       *(array3 + k) = *c;
       k++;
     } else {
@@ -139,11 +143,11 @@ void bSort(char *begin, const char *const end, Comp_char func = More) {
   Sort(array3, array3 + size_arr3, func);
   Sort(array4, array4 + size_arr4, func);
   char *tmp_string = new char[size_arr1 + size_arr2 + size_arr3 + size_arr4];
-  std::strncpy(tmp_string, array1, size_arr1);
-  std::strncat(tmp_string, array2, size_arr2);
-  std::strncat(tmp_string, array3, size_arr3);
-  std::strncat(tmp_string, array4, size_arr4);
-  std::strncpy(begin, tmp_string, size_arr1 + size_arr2 + size_arr3 + size_arr4);
+  strncpy(tmp_string, array1, size_arr1);
+  strncat(tmp_string, array2, size_arr2);
+  strncat(tmp_string, array3, size_arr3);
+  strncat(tmp_string, array4, size_arr4);
+  strncpy(begin, tmp_string, size_arr1 + size_arr2 + size_arr3 + size_arr4);
   delete[] tmp_string;
   delete[] array1;
   delete[] array2;
