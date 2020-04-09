9571fec094d0bf6948c8fbf53fe5f7fbe01df2c5
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-09 14:09:21 +0300

    Update Alex_Zubok_home_work3_ad4.cpp

--- HomeWork3/Alex_Zubok_home_work3_ad4.cpp
+++ HomeWork3/Alex_Zubok_home_work3_ad4.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 
 void lower_string(char *string) {
-  for (size_t i = 0; i < std::strlen(string); i++) {
-    if (*(string + i) > 64 && *(string + i) < 89)
+  if (string == nullptr || string + strlen(string) == nullptr ||
+      strlen(string) == 0)
+    return;
+  for (size_t i = 0; i < strlen(string); i++) {
+    if (*(string + i) >= 'A' && *(string + i) <= 'Z')
       *(string + i) += 32;
     std::cout << *(string + i);
   }
@@ -11,24 +14,30 @@ void lower_string(char *string) {
 }
 
 char *ret_lower_string(char *string) {
-  for (size_t i = 0; i < std::strlen(string); i++) {
-    if (*(string + i) > 64 && *(string + i) < 89)
+  if (string == nullptr || string + strlen(string) == nullptr ||
+      strlen(string) == 0)
+    return;
+  for (size_t i = 0; i < strlen(string); i++) {
+    if (*(string + i) >= 'A' && *(string + i) <= 'Z')
       *(string + i) += 32;
   }
   return string;
 }
 
 int main() {
-  std::cout << "Please enter string: ";
-  char *tmp_string = new char;
-  std::cin >> tmp_string;
-  char *string = new char[std::strlen(tmp_string) + 1];
-  std::strcpy(string, tmp_string);
-  delete tmp_string;
+  const size_t kMaxStringSize = 1000;
+  const char kEnter = '\n';
+  std::cout << "Please enter string to : ";
+  char *tmp_string = new char[kMaxStringSize];
+  std::cin.get(tmp_string, kMaxStringSize, kEnter);
+  char *string = new char[strlen(tmp_string) + 1];
+  strcpy(string, tmp_string);
+  delete[] tmp_string;
   std::cout << "Your string is: " << string << std::endl;
   std::cout << "Your lower string is: ";
   lower_string(string);
-  std::cout << "Your lower return string is " << ret_lower_string(string) << std::endl;
+  std::cout << "Your lower return string is " << ret_lower_string(string)
+            << std::endl;
   delete[] string;
   return 0;
 }
