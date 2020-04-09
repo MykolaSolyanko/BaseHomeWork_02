de7655126eb96d5aa25ae0762529cfb8a65cffa4
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-09 15:13:07 +0300

    Update Alex_Zubok_home_work3_ad2.cpp

--- HomeWork3/Alex_Zubok_home_work3_ad2.cpp
+++ HomeWork3/Alex_Zubok_home_work3_ad2.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 
 void upper_string(char *string) {
-  for (size_t i = 0; i < std::strlen(string); i++) {
-    if (*(string + i) > 96 && *(string + i) < 123)
+  if (string == nullptr || string + strlen(string) == nullptr ||
+      strlen(string) == 0)
+    return;
+  for (size_t i = 0; i < strlen(string); i++) {
+    if (*(string + i) >= 'a' && *(string + i) <= 'z')
       *(string + i) -= 32;
     std::cout << *(string + i);
   }
@@ -11,24 +14,30 @@ void upper_string(char *string) {
 }
 
 char *ret_upper_string(char *string) {
-  for (size_t i = 0; i < std::strlen(string); i++) {
-    if (*(string + i) > 96 && *(string + i) < 123)
+  if (string == nullptr || string + strlen(string) == nullptr ||
+      strlen(string) == 0)
+    return string;
+  for (size_t i = 0; i < strlen(string); i++) {
+    if (*(string + i) >= 'a' && *(string + i) <= 'z')
       *(string + i) -= 32;
   }
   return string;
 }
 
 int main() {
+  const size_t kMaxStringSize = 1000;
+  const char kEnter = '\n';
   std::cout << "Please enter string: ";
-  char *tmp_string = new char;
-  std::cin >> tmp_string;
-  char *string = new char[std::strlen(tmp_string) + 1];
-  std::strcpy(string, tmp_string);
-  delete tmp_string;
+  char *tmp_string = new char[kMaxStringSize]{};
+  std::cin.get(tmp_string, kMaxStringSize, kEnter);
+  char *string = new char[strlen(tmp_string) + 1]{};
+  strncpy(string, tmp_string, strlen(tmp_string));
+  delete[] tmp_string;
   std::cout << "Your string is: " << string << std::endl;
   std::cout << "Your upper string is: ";
   upper_string(string);
-  std::cout << "Your upper return string is " << ret_upper_string(string) << std::endl;
+  std::cout << "Your upper return string is " << ret_upper_string(string)
+            << std::endl;
   delete[] string;
   return 0;
 }
