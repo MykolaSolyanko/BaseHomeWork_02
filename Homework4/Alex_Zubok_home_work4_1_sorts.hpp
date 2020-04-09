daf58a55324752bbbac2a7e60199260095c4f9f0
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-09 18:03:28 +0300

    Update Alex_Zubok_home_work4_1_sorts.hpp

--- Homework4/Alex_Zubok_home_work4_1_sorts.hpp
+++ Homework4/Alex_Zubok_home_work4_1_sorts.hpp
@@ -6,8 +6,9 @@
 
 using Comp = bool (*)(int, int);
 bool More(int a, int b);
-bool Less(int a, int b);
-void bSort(int *begin, const int *const end, Comp func = More);
+void bSort(int *begin, int *end, Comp func = More);
 int *user_partiotion(int *begin, int *end, Comp func = More);
 void qSort(int *begin, int *end, Comp func = More);
 void print_array(const int *begin, const int *const end);
+
+enum use_choise { eManual = 1, eAuto };
