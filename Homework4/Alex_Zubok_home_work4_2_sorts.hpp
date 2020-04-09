179325036ad0879849704e55988aff2603724415
AlexZubok <60966282+AlexZubok@users.noreply.github.com>
2020-04-09 19:04:55 +0300

    Update Alex_Zubok_home_work4_2_sorts.hpp

--- Homework4/Alex_Zubok_home_work4_2_sorts.hpp
+++ Homework4/Alex_Zubok_home_work4_2_sorts.hpp
@@ -4,23 +4,21 @@
 #include <iostream>
 #include <random>
 
-using Comp_unsigned = bool (*)(unsigned, unsigned);
 bool More(unsigned a, unsigned b);
-bool Less(unsigned a, unsigned b);
+using Comp_unsigned = bool (*)(unsigned, unsigned);
 int *user_partiotion(unsigned *begin, unsigned *end, Comp_unsigned func = More);
 void qSort(unsigned *begin, unsigned *end, Comp_unsigned func = More);
 void print_array(const unsigned *begin, const unsigned *const end);
 
 using Comp_double = bool (*)(double, double);
 bool More(double a, double b);
-bool Less(double a, double b);
 int *user_partiotion(double *begin, double *end, Comp_double func = More);
 void qSort(double *begin, double *end, Comp_double func = More);
 void print_array(const double *begin, const double *const end);
 
 using Comp_char = bool (*)(char, char);
 bool More(char a, char b);
-bool Less(char a, char b);
 int *user_partiotion(char *begin, char *end, Comp_char func = Less);
 void bSort(char *begin, const char *const end, Comp_char func = Less);
 void print_array(const char *begin, const char *const end);
+enum user_choise { eManual = 1, eAuto };
