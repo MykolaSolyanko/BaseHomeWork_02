c4b88cd534c30811c3b123bc9096c53c11f0998c
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-04-11 15:30:18 +0300

    Update Maksim_Panibrat_HW7_BubbleSort_Source.cpp

--- HomeWork7/Maksim_Panibrat_HW7_BubbleSort_Source.cpp
+++ HomeWork7/Maksim_Panibrat_HW7_BubbleSort_Source.cpp
@@ -3,7 +3,7 @@
 const size_t kSize = 20;
 
 template <typename T>
-static void Array_result(T *begin, const T *end, const size_t kSize) {
+void Array_result(T *begin, const T *end, const size_t kSize) {
   std::cout << "result: array[" << kSize << "]{";
   for (begin; begin != end; ++begin) {
     std::cout << *begin << " ";
@@ -11,7 +11,7 @@ static void Array_result(T *begin, const T *end, const size_t kSize) {
   std::cout << "}\n";
 }
 
-template <typename T> static void Swap(T *lhs, T *rhs) {
+template <typename T> void Swap(T *lhs, T *rhs) {
   std::cout << "swaping : lhs" << *lhs << ", rhs " << *rhs << "\n";
   T tmp = *lhs;
   *lhs = *rhs;
@@ -19,13 +19,10 @@ template <typename T> static void Swap(T *lhs, T *rhs) {
   std::cout << "swap result: lhs" << *lhs << ", rhs " << *rhs << "\n";
 }
 
-template <typename T> static void Bubble_sort(T *begin, const T *end) {
-  auto comp = [](T a, T b) {
-    return a < b;
-  }; /*sort less to left side  return a < b
-       sort less to right side return a > b*/
+template <typename T>
+void Bubble_sort(T *begin, const T *end, Comparison comp) {
   for (T *bubble = begin; bubble != end; ++bubble) {
-    for (T *next_bubble = begin; next_bubble != end; ++next_bubble) {
+    for (T *next_bubble = bubble + 1; next_bubble != end; ++next_bubble) {
       if (comp(*bubble, *next_bubble)) {
         Swap(bubble, next_bubble);
       }
@@ -33,7 +30,33 @@ template <typename T> static void Bubble_sort(T *begin, const T *end) {
   }
 }
 
-template <typename T> void Random_natural_array(T *begin, const T *end) {
+template <typename T> void Sort_settings(T *begin, T *end) {
+  std::cout << "Select sort side: \n"
+               "\t1 - left\n"
+               "\t2 - right\n";
+  short select;
+  std::cin >> select;
+  switch (select) {
+  case (LEFT): {
+    auto comp = [](T a, T b) { return a > b; };
+    Bubble_sort(begin, end, comp);
+    break;
+  }
+  case (RIGHT): {
+    auto comp = [](T a, T b) { return a < b; };
+    Bubble_sort(begin, end, comp);
+    break;
+  }
+  default: {
+    std::cout << "Error, using default-left side.\n";
+    auto comp = [](T a, T b) { return a > b; };
+    Bubble_sort(begin, end, comp);
+    break;
+  }
+  }
+}
+
+template <typename T> void Random_whole_array(T *begin, const T *end) {
   std::mt19937 generator{};
   T kMin, kMax;
   std::cout << "Enter min number: ";
@@ -41,72 +64,76 @@ template <typename T> void Random_natural_array(T *begin, const T *end) {
   std::cout << "Enter max number: ";
   std::cin >> kMax;
   std::uniform_int_distribution<T> distribution(kMin, kMax);
-  for (begin; begin != end; ++begin) {
+  for (; begin != end; ++begin) {
     *begin = distribution(generator);
   }
 }
 
-void Create_natural() {
-    std::cout << "Select real number type: \n"
-        "\t1 - short\n"
-        "\t2 - int\n"
-        "\t3 - long\n";
-    int select;
-    std::cin >> select;
-    switch (select) {
-    case(1): {
-        short array[kSize]{};
-        short* begin = { array };
-        short* end = begin + kSize;
-        Random_natural_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+void Create_whole() {
+  std::cout << "Select real number type: \n"
+               "\t1 - short\n"
+               "\t2 - int\n"
+               "\t3 - long\n";
+  short select;
+  std::cin >> select;
+  switch (select) {
+  case (SHORT): {
+    short array[kSize]{};
+    short *begin = {array};
+    short *end = begin + kSize;
+    Random_whole_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    case(2): {
-        int array[kSize]{};
-        int* begin = { array };
-        int* end = begin + kSize;
-        Random_natural_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  case (INT): {
+    int array[kSize]{};
+    int *begin = {array};
+    int *end = begin + kSize;
+    Random_whole_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    case(3): {
-        long array[kSize]{};
-        long* begin = { array };
-        long* end = begin + kSize;
-        Random_natural_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  case (LONG): {
+    long array[kSize]{};
+    long *begin = {array};
+    long *end = begin + kSize;
+    Random_whole_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    default: {
-        std::cout << "Error, using default int type.\n";
-        int array[kSize]{};
-        int* begin = { array };
-        int* end = begin + kSize;
-        Random_natural_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  default: {
+    std::cout << "Error, using default int type.\n";
+    int array[kSize]{};
+    int *begin = {array};
+    int *end = begin + kSize;
+    Random_whole_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    }
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  }
 }
 
-template <typename T> void Random_real_array(T *begin, const T *end) {
+template <typename T> void Random_fractional_array(T *begin, const T *end) {
   std::mt19937 generator{};
   T kMin, kMax;
   std::cout << "Enter min number: ";
@@ -114,65 +141,68 @@ template <typename T> void Random_real_array(T *begin, const T *end) {
   std::cout << "Enter max number: ";
   std::cin >> kMax;
   std::uniform_real_distribution<T> distribution(kMin, kMax);
-  for (begin; begin != end; ++begin) {
+  for (; begin != end; ++begin) {
     *begin = distribution(generator);
   }
 }
 
-void Create_real() {
-    std::cout << "Select real number type: \n"
-        "\t1 - float\n"
-        "\t2 - double";
-    int select;
-    std::cin >> select;
-    switch (select) {
-    case(1): {
-        float array[kSize]{};
-        float* begin = { array };
-        float* end = begin + kSize;
-        Random_real_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+void Create_fractional() {
+  std::cout << "Select real number type: \n"
+               "\t1 - float\n"
+               "\t2 - double";
+  int select;
+  std::cin >> select;
+  switch (select) {
+  case (FLOAT): {
+    float array[kSize]{};
+    float *begin = {array};
+    float *end = begin + kSize;
+    Random_fractional_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    case(2): {
-        double array[kSize]{};
-        double* begin = { array };
-        double* end = begin + kSize;
-        Random_real_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  case (DOUBLE): {
+    double array[kSize]{};
+    double *begin = {array};
+    double *end = begin + kSize;
+    Random_fractional_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    default: {
-        std::cout << "Error, using default float type.\n";
-        float array[kSize]{};
-        float* begin = { array };
-        float* end = begin + kSize;
-        Random_real_array(begin, end);
-        std::cout << "Random input ";
-        Array_result(begin, end, kSize);
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  default: {
+    std::cout << "Error, using default float type.\n";
+    float array[kSize]{};
+    float *begin = {array};
+    float *end = begin + kSize;
+    Random_fractional_array(begin, end);
+    std::cout << "Random input ";
+    Array_result(begin, end, kSize);
 
-        Bubble_sort(begin, end);
-        std::cout << "Bubble sort ";
-        Array_result(begin, end, kSize);
-        break;}
-    }
+    Sort_settings(begin, end);
+    std::cout << "Bubble sort ";
+    Array_result(begin, end, kSize);
+    break;
+  }
+  }
 }
 
 void Random_char_array(char *begin, const char *end) {
   std::mt19937 generator{};
-  int kUpperMin = 65, kUpperMax = 90;
+  int kUpperMin = 'A', kUpperMax = 'Z';
   std::uniform_int_distribution<int> distribution_upper(kUpperMin, kUpperMax);
-  int kLowerMin = 97, kLowerMax = 122;
+  int kLowerMin = 'a', kLowerMax = 'z';
   std::uniform_int_distribution<int> distribution_lower(kLowerMin, kLowerMax);
-  std::uniform_int_distribution<int> distribiton_switch(0, 1);
+  std::uniform_int_distribution<int> distribiton_switch(UPPER, LOWER);
   for (; begin != end; ++begin) {
     *begin = distribiton_switch(generator) ? distribution_upper(generator)
                                            : distribution_lower(generator);
@@ -188,7 +218,7 @@ void Create_char() {
   std::cout << array;
   Array_result(begin, end, kSize);
 
-  Bubble_sort(begin, end);
+  Sort_settings(begin, end);
   std::cout << "Bubble sort ";
   Array_result(begin, end, kSize);
 }
