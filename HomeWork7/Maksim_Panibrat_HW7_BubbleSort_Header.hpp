8499089037c315ba319ce0c3b3eaf577ba70c566
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-04-11 15:29:17 +0300

    Update Maksim_Panibrat_HW7_BubbleSort_Header.hpp

--- HomeWork7/Maksim_Panibrat_HW7_BubbleSort_Header.hpp
+++ HomeWork7/Maksim_Panibrat_HW7_BubbleSort_Header.hpp
@@ -3,6 +3,18 @@
 #include <iostream>
 #include <random>
 
-void Create_natural();
-void Create_real();
+using Comparison = std::function<bool(int, int)>;
+
+enum SELECT_TYPE { WHOLE = 1, FRACTIONAL, LETTERS };
+
+enum SELECT_WHOLE { SHORT = 1, INT, LONG };
+
+enum SELECT_FRACTIONAL { FLOAT = 1, DOUBLE };
+
+enum SELECT_CHAR { UPPER = 0, LOWER = 1 };
+
+enum SELECT_SIDE { LEFT = 1, RIGHT };
+
+void Create_whole();
+void Create_fractional();
 void Create_char();
