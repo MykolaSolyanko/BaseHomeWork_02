87b60b6d2879f28c9da95d2b58d9ae10a8275417
NataliiaKuchinska <60893756+NataliiaKuchinska@users.noreply.github.com>
2020-04-11 23:51:28 +0300

    Updated

--- HomeWork6/Nataliia_Kychynska_HW6.cpp
+++ HomeWork6/Nataliia_Kychynska_HW6.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 
-class queue {
+class Queue {
 public:
   bool is_empty();
   void push(int elmnt);
   int pop();
   void clear();
-  const void print();
   const int get_qsize();
-  const int get_MAX_qsize();
+  const int get_qcapacity();
   const int get_first_elmnt();
+  ~Queue();
 
 private:
   static const int qSize{1000};
-  int queue_str[qSize];
-  int first_pos{1};
+  int *queue_str = new int[qSize];
+  int first_pos{0};
   int last_pos{0};
 };
-bool queue::is_empty() { return ((last_pos < first_pos) ? 1 : 0); }
-void queue::push(int elmnt) {
-  if (last_pos < qSize - 1) {
-    last_pos++;
-    queue_str[last_pos] = elmnt;
+
+Queue::~Queue() { delete[] queue_str; };
+bool Queue::is_empty() { return (last_pos == 0) && (first_pos == 0); }
+void Queue::push(int elmnt) {
+  if (last_pos < qSize) {
+    queue_str[last_pos++] = elmnt;
   } else {
     std::cout << " Queue is full. " << std::endl;
   }
 };
-int queue::pop() {
+int Queue::pop() {
   if (is_empty()) {
     std::cout << "Queue is empty" << std::endl;
     return 0;
@@ -38,23 +39,14 @@ int queue::pop() {
   last_pos--;
   return elmnt;
 }
-void queue::clear() {
-  first_pos = 1;
+void Queue::clear() {
+  first_pos = 0;
   last_pos = 0;
 }
-const void queue::print() {
-  if (is_empty()) {
-    std::cout << "Queue is empty. No elements to print. " << std::endl;
-  } else {
-    std::cout << "queue state " << std::endl;
-    for (int i = first_pos; i <= last_pos; ++i)
-      std::cout << queue_str[i] << " ";
-  }
-  std::cout << std::endl;
-}
-const int queue::get_qsize() { return last_pos; }
-const int queue::get_MAX_qsize() { return qSize; }
-const int queue::get_first_elmnt() {
+
+const int Queue::get_qsize() { return last_pos; }
+const int Queue::get_qcapacity() { return qSize; }
+const int Queue::get_first_elmnt() {
   if (is_empty()) {
     std::cout << "Queue is empty." << std::endl;
     return 0;
@@ -64,20 +56,16 @@ const int queue::get_first_elmnt() {
 }
 
 int main() {
-  queue q1;
-  std::cout << q1.get_MAX_qsize() << std::endl;
-  q1.print();
+  Queue q1;
+  std::cout << q1.get_qcapacity() << std::endl;
   q1.push(10);
-
-  q1.print();
   q1.push(20);
   q1.push(30);
   std::cout << "Queue is empty? " << q1.is_empty() << std::endl;
   std::cout << "Get size: " << q1.get_qsize() << std::endl;
-  q1.print();
+  std::cout << "1st element" << q1.get_first_elmnt() << std::endl;
   q1.pop();
-  q1.print();
   q1.clear();
-  q1.print();
+
   return 0;
 }
