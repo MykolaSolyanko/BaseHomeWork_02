637aac046cd68df85e2babaedf33ae997c1dd904
PavelGenitsoy <60933533+PavelGenitsoy@users.noreply.github.com>
2020-04-13 02:39:22 +0300

    fixed

--- HomeWork6/PavelGenitsoyHW6stack.cpp
+++ HomeWork6/PavelGenitsoyHW6stack.cpp
@@ -1,53 +1,49 @@
 #include "Stack.hpp"
 #include <iostream>
 
-Stack::Stack(size_t size) : kSize(size) { array = new int[kSize]{}; }
-
-Stack::~Stack() { delete[] array; }
+static const auto kMax_Size_of_stack{100};
 
 bool Stack::push(int value) {
-  if (index == 100) {
+  if (index == kMax_Size_of_stack) {
     return false;
   }
   array[index++] = value;
   return true;
 }
 
-std::pair<int, bool> Stack::pop() {
+bool Stack::pop() {
   if (index == 0) {
-    return {0, false};
+    return false;
   }
-  return {array[--index], true};
+  --index;
+  std::cout << __FUNCTION__ << std::endl;
+  return true;
 }
 
 void Stack::clear() {
-  if (index == 0) {
-    return;
-  }
   index = 0;
+  std::cout << __FUNCTION__ << std::endl;
 }
 
 bool Stack::empty() const { return index == 0; }
 
-size_t Stack::GetStackSize() const { return kSize; }
-
-std::pair<int, bool> Stack::top() {
+int Stack::top() {
   if (index == 0) {
-    return {0, false};
+    return 0;
   }
-  return {array[index - 1], true};
+  return array[index - 1];
 }
 
-std::pair<int, bool> Stack::GetStackLength() const {
+size_t Stack::GetStackLength() {
   if (index == 0) {
-    return {0, false};
+    return 0;
   }
   int count{0};
   size_t virt_index{index};
   while (virt_index-- != 0) {
     count++;
   }
-  return {count, true};
+  return count;
 }
 
 void Stack::print() const {
@@ -59,24 +55,4 @@ void Stack::print() const {
     std::cout << array[--virt_index] << " ";
   }
   std::cout << std::endl;
-}
-
-int Stack::find(int value) {
-  if (empty()) {
-    return 0;
-  }
-  size_t virt_index{index};
-  while (virt_index != 0) {
-    --virt_index;
-    if (virt_index == 0 && array[virt_index] != value) {
-      std::cout << "\nNot found\n";
-      return virt_index;
-    }
-    if (array[virt_index] == value) {
-      std::cout << "\nFound. The index of this element in stack is "
-                << virt_index << std::endl;
-      return array[virt_index];
-    }
-  }
-  return virt_index;
 }
