567711540c430cdb3f61c9cfadb0443b0e817fed
PavelGenitsoy <60933533+PavelGenitsoy@users.noreply.github.com>
2020-04-13 02:38:45 +0300

    fixed

--- HomeWork6/PavelGenitsoyHW6stack.hpp
+++ HomeWork6/PavelGenitsoyHW6stack.hpp
@@ -1,22 +1,17 @@
 #pragma once
-#include <utility>
 
 class Stack {
 public:
-  Stack(size_t size);
-  ~Stack();
   bool push(int value);
-  std::pair<int, bool> pop();
+  bool pop();
   void clear();
   bool empty() const;
-  size_t GetStackSize() const;
-  std::pair<int, bool> top();
-  std::pair<int, bool> GetStackLength() const;
+  int top();
+  size_t GetStackLength();
   void print() const;
-  int find(int value);
 
 private:
-  const size_t kSize{100};
+  static const size_t kSize{100};
   size_t index{};
-  int *array{nullptr};
+  int array[kSize]{};
 };
