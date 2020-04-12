46405c989457ca2cb8a9dc47bf4e51ffa6556f86
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-04-12 22:31:04 +0300

    Update Maksim_Panibrat_HW7_Stack_Header.hpp

--- HomeWork7/Maksim_Panibrat_HW7_Stack_Header.hpp
+++ HomeWork7/Maksim_Panibrat_HW7_Stack_Header.hpp
@@ -7,23 +7,29 @@ struct Pair {
 	const char* msg;
 };
 
+template <typename T>
+struct Value {
+	bool status;
+	T msg;
+};
+
 template <typename T, const long long kSize = 5>
 class Stack {
 public:
 	Pair push(T value, Pair pair) {
 		std::cout << "Index: " << index << std::endl;
 		if (index == kSize) {
-			return {false, "Stack full" };
+			return { false, "Stack full" };
 		}
 		array[++index] = value;
-		return {true, "Push success" };
+		return { true, "Push success" };
 	}
 	Pair pop(Pair pair) {
 		if (index == 0) {
 			return { false, "Stack clear" };
 		}
 		array[index--];
-		return {true, "Pop success"};
+		return { true, "Pop success" };
 	}
 	void clear() {
 		if (index == 0) {
@@ -31,11 +37,16 @@ public:
 		}
 		index = 0;
 	}
-	T top() const {
+	
+	template <typename T>
+	Value <T> top() const {
+		Value <T> topy{ false, 0 };
 		if (index == 0) {
-			return 0;
+			return topy;
 		}
-		return array[index];
+		topy.status = true;
+		topy.msg = array[index];
+		return topy;
 	}
 
 	const unsigned long long GetSizeStack() {
