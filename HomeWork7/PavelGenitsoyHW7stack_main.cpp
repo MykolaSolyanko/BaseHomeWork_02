54e2d1ae9285698e220c1e5d0d2fd55186b64676
PavelGenitsoy <60933533+PavelGenitsoy@users.noreply.github.com>
2020-04-13 02:50:32 +0300

    fixed

--- HomeWork7/PavelGenitsoyHW7stack_main.cpp
+++ HomeWork7/PavelGenitsoyHW7stack_main.cpp
@@ -2,17 +2,9 @@
 #include <iostream>
 
 int main() {
-  Stack<int, bool> s(50);
+  Stack<int, bool> s;
   if (s.empty()) {
-    std::cout << "\nStack is empty\n";
-  }
-
-  std::cout << "\nSize (constant) of stack: " << s.GetStackSize() << std::endl;
-
-  if (auto [value, status] = s.top(); status == true) {
-    std::cout << value << std::endl;
-  } else {
-    std::cout << "\nerror top\n" << std::endl;
+    std::cout << "Stack is empty\n" << std::endl;
   }
   if (s.push(5)) {
     std::cout << "push(5) was success\n" << std::endl;
@@ -23,26 +15,19 @@ int main() {
   if (s.push(20)) {
     std::cout << "push(10) was success\n" << std::endl;
   }
-  if (auto [value, status] = s.GetStackLength(); status == true) {
-    std::cout << "Length of stack: " << value << std::endl;
-  }
+
+  std::cout << "Length of stack: " << s.GetStackLength() << std::endl;
+
   std::cout << "\nContent of stack: ";
   s.print();
-  if (auto [value, status] = s.top(); status == true) {
-    std::cout << "\nValue from top: " << value << std::endl;
-  }
-  if (auto [value, status] = s.pop(); status == true) {
-    std::cout << "\nValue which we deleted from stack: " << value << std::endl;
-  }
+
+  std::cout << "\nValue from top: " << s.top() << std::endl;
+
+  s.pop();
   std::cout << "\nContent of stack: ";
   s.print();
-  if (auto [value, status] = s.top(); status == true) {
-    std::cout << "\nValue from top: " << value << std::endl;
-  }
-  std::cout << "\nWe want to find the element [10] int the stack: ";
-  s.find(10);
-  std::cout << "\nWe want to find the element [15] int the stack: ";
-  s.find(15);
+
+  s.clear();
 
   system("pause");
   return 0;
