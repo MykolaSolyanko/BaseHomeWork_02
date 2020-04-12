4a300211d57f860cc0ecd2325a28ebdc54c4021d
PavelGenitsoy <60933533+PavelGenitsoy@users.noreply.github.com>
2020-04-13 02:49:55 +0300

    fixed

--- HomeWork7/PavelGenitsoyHW7stack.hpp
+++ HomeWork7/PavelGenitsoyHW7stack.hpp
@@ -1,83 +1,66 @@
 #pragma once
 #include <iostream>
-#include <utility>
 
 template <typename T1, typename T2> class Stack {
 public:
-  Stack() = default;
-  Stack(size_t size);
-  ~Stack();
   T2 push(T1 value);
-  std::pair<T1, T2> pop();
+  T2 pop();
   void clear();
   T2 empty() const;
-  size_t GetStackSize() const;
-  std::pair<T1, T2> top();
-  std::pair<T1, T2> GetStackLength() const;
+  T1 top();
+  size_t GetStackLength() const;
   void print() const;
-  T1 find(T1 value);
 
 private:
-  const size_t kSize{100};
+  static const auto kMax_Size_of_stack{100};
   size_t index{};
-  T1 *array{nullptr};
+  T1 array[kMax_Size_of_stack]{};
 };
 
-template <typename T1, typename T2>
-Stack<T1, T2>::Stack(size_t size) : kSize(size) {
-  array = new T1[kSize]{};
-}
-
-template <typename T1, typename T2> Stack<T1, T2>::~Stack() { delete[] array; }
-
 template <typename T1, typename T2> T2 Stack<T1, T2>::push(T1 value) {
-  if (index == 100) {
+  if (index == kMax_Size_of_stack) {
     return false;
   }
   array[index++] = value;
   return true;
 }
 
-template <typename T1, typename T2> std::pair<T1, T2> Stack<T1, T2>::pop() {
+template <typename T1, typename T2> T2 Stack<T1, T2>::pop() {
   if (index == 0) {
-    return {0, false};
+    return false;
   }
-  return {array[--index], true};
+  --index;
+  std::cout << __FUNCTION__ << std::endl;
+  return true;
 }
 
 template <typename T1, typename T2> void Stack<T1, T2>::clear() {
-  if (index == 0) {
-    return;
-  }
   index = 0;
+  std::cout << __FUNCTION__ << std::endl;
 }
 
 template <typename T1, typename T2> T2 Stack<T1, T2>::empty() const {
   return index == 0;
 }
 
-template <typename T1, typename T2> size_t Stack<T1, T2>::GetStackSize() const {
-  return kSize;
-}
-
-template <typename T1, typename T2> std::pair<T1, T2> Stack<T1, T2>::top() {
+template <typename T1, typename T2> T1 Stack<T1, T2>::top() {
   if (index == 0) {
-    return {0, false};
+    return 0;
   }
-  return {array[index - 1], true};
+  return array[index - 1];
 }
 
 template <typename T1, typename T2>
-std::pair<T1, T2> Stack<T1, T2>::GetStackLength() const {
+size_t Stack<T1, T2>::GetStackLength() const {
   if (index == 0) {
-    return {0, false};
+    return 0;
   }
   int count{0};
   size_t virt_index{index};
   while (virt_index-- != 0) {
     count++;
   }
-  return {count, true};
+  return count;
 }
 
 template <typename T1, typename T2> void Stack<T1, T2>::print() const {
@@ -89,24 +72,4 @@ template <typename T1, typename T2> void Stack<T1, T2>::print() const {
     std::cout << array[--virt_index] << " ";
   }
   std::cout << std::endl;
-}
-
-template <typename T1, typename T2> T1 Stack<T1, T2>::find(T1 value) {
-  if (empty()) {
-    return 0;
-  }
-  size_t virt_index{index};
-  while (virt_index != 0) {
-    --virt_index;
-    if (virt_index == 0 && array[virt_index] != value) {
-      std::cout << "\nNot found\n";
-      return virt_index;
-    }
-    if (array[virt_index] == value) {
-      std::cout << "\nFound. The index of this element in stack is "
-                << virt_index << std::endl;
-      return array[virt_index];
-    }
-  }
-  return virt_index;
 }
