999928c412d5ebfb6c45e6e6e834218f0cb2a819
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-04-12 22:43:28 +0300

    Update Maksim_Panibrat_HW7_Stack_main.cpp

--- HomeWork7/Maksim_Panibrat_HW7_Stack_main.cpp
+++ HomeWork7/Maksim_Panibrat_HW7_Stack_main.cpp
@@ -1,78 +1,81 @@
 #include "Header.h"
 
-int main() {
-  std::cout << "This program realize Stack by using template. \n";
+int main()
+{
+    std::cout << "This program realize Stack by using template. \n";
 
-  {
-    Stack<int> st_int;
-    std::cout << "Stack size: " << st_int.GetSizeStack() << std::endl;
     {
-      std::cout << "Int Stack\n";
-      Pair pair{true, ""};
-      while (pair.status == true) {
-        int value;
-        std::cin >> value;
-        pair = st_int.push(value, pair);
-        Value<int> topy = st_int.top<int>();
-        std::cout << "Pushed: " << topy.msg << std::endl;
-      }
-      std::cout << pair.msg << std::endl;
+        Stack <int> st_int;
+        std::cout << "Stack size: " << st_int.GetSizeStack() << std::endl;
+        Pair pair{ true , "" };
+        Value <int> topy;
+        {
+            std::cout << "Int Stack\n";
+            while (pair.status == true) {
+                int value;
+                std::cin >> value;
+                pair = st_int.push(value, pair);
+                topy = st_int.top<int>();
+                std::cout << "Pushed: " << topy.msg << std::endl;
+            }
+            std::cout << pair.msg << std::endl;
+        }
+        {
+            for (Pair pair{ true , "" };pair.status == true;pair = st_int.pop(pair)) {
+                topy = st_int.top<int>();
+                std::cout << "Popped: " << topy.msg << std::endl;
+            }
+        }
+        st_int.clear();
+        topy = st_int.top<int>();
+        std::cout << "new top after clear: " << topy.msg << std::endl;
     }
-    {
-      for (Pair pair{true, ""}; pair.status == true; pair = st_int.pop(pair)) {
-        Value<int> topy = st_int.top<int>();
-        std::cout << "Popped: " << topy.msg << std::endl;
-      }
-    }
-    st_int.clear();
-    Value<int> topy = st_int.top<int>();
-    std::cout << "new top after clear: " << topy.msg << std::endl;
-  }
 
-  {
-    Stack<float> st_float;
     {
-      std::cout << "float Stack\n";
-      Pair pair{true, ""};
-      while (pair.status == true) {
-        float value;
-        std::cin >> value;
-        pair = st_float.push(value, pair);
-        Value<float> topy = st_float.top<float>();
-        std::cout << "Pushed: " << topy.msg << std::endl;
-      }
-      std::cout << pair.msg << std::endl;
+        Stack <float> st_float;
+        Pair pair{ true , "" };
+        Value <float> topy;
+        {
+            std::cout << "float Stack\n";
+            while (pair.status == true) {
+                float value;
+                std::cin >> value;
+                pair = st_float.push(value, pair);
+                topy = st_float.top<float>();
+                std::cout << "Pushed: " << topy.msg << std::endl;
+            }
+            std::cout << pair.msg << std::endl;
+        }
+        {
+            for (Pair pair{ true , "" };pair.status == true;pair = st_float.pop(pair)) {
+                topy = st_float.top<float>();
+                std::cout << "Popped: " << topy.msg << std::endl;
+            }
+        }
     }
-    {
-      for (Pair pair{true, ""}; pair.status == true;
-           pair = st_float.pop(pair)) {
-        Value<float> topy = st_float.top<float>();
-        std::cout << "Popped: " << topy.msg << std::endl;
-      }
-    }
-  }
 
-  {
-    Stack<char> st_char;
     {
-      std::cout << "char Stack\n";
-      Pair pair{true, ""};
-      while (pair.status == true) {
-        char value;
-        std::cin >> value;
-        pair = st_char.push(value, pair);
-        Value<char> topy = st_char.top<char>();
-        std::cout << "Pushed: " << topy.msg << std::endl;
-      }
-      std::cout << pair.msg << std::endl;
+        Stack <char> st_char;
+        Pair pair{ true , "" };
+        Value <char> topy;
+        {
+            std::cout << "char Stack\n";
+            while (pair.status == true) {
+                char value;
+                std::cin >> value;
+                pair = st_char.push(value, pair);
+                topy = st_char.top<char>();
+                std::cout << "Pushed: " << topy.msg << std::endl;
+            }
+            std::cout << pair.msg << std::endl;
+        }
+        {
+            for (Pair pair{ true , "" };pair.status == true;pair = st_char.pop(pair)) {
+                topy = st_char.top<char>();
+                std::cout << "Popped: " << topy.msg << std::endl;
+            }
+        }
     }
-    {
-      for (Pair pair{true, ""}; pair.status == true; pair = st_char.pop(pair)) {
-        Value<char> topy = st_char.top<char>();
-        std::cout << "Popped: " << topy.msg << std::endl;
-      }
-    }
-  }
 
-  return 0;
+    return 0;
 }
